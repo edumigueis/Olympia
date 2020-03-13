bf9336522916df2ac5373b96f524cd52095c2ee8
digo-smithh <smithrodrigues08@gmail.com>
2020-03-13 00:30:05 -0300

    df

--- Olympia/public/js/home.js
+++ Olympia/public/js/home.js
@@ -1,13 +1,30 @@
-var i = 0;
+var i = -1;
 var y = 10;
 var x = 0;
-var max = 66; /* NÚMERO TOTAL DE CÍRCULOS */
+var aux = false;
+var passou = false;
+var max = 84; /* NÚMERO TOTAL DE CÍRCULOS */
 
 setInterval(function () {
-    $("." + i).fadeIn(150); /* TEMPO DE APARIÇÃO DOS CÍRCULOS */
-    if (i < max)
-        i++;
-}, 100); /* INTERVALO ENTRE AS APARIÇÕES */
+
+    if (i < max) {
+        if (passou) {
+            i--;
+            if (i % 14 == 0 && i != 0) {
+                passou = false;
+                i += 15;
+            }
+        }
+        else {
+            i++;
+        }
+        if (i % 14 == 0 && i != 0) {
+            i += 13;
+            passou = true;
+        }
+    }
+    $("." + i).fadeIn(500); /* TEMPO DE APARIÇÃO DOS CÍRCULOS */
+}, 50); /* INTERVALO ENTRE AS APARIÇÕES */
 
 $().ready(function () {
 
@@ -17,7 +34,7 @@ $().ready(function () {
     $('circle').attr("stroke", 'black');
     $('circle').attr("stroke-width", '0.1');
     /**/
-                        
+
     for (var i2 = 0; i2 < max; i2++) {
 
         x++;
@@ -37,5 +54,5 @@ $().ready(function () {
         $('.' + i2).attr('cy', y);
     }
 
-    $('.' + max).css('display','none');
+    $('.' + max).css('display', 'none');
 })
