8c625b89e4e3e556b70ecac920994c2133dbf80b
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-03-30 23:29:40 -0300

    Implementado o upload de imagens para o cadastro local. E funções lógica de cadastro.s

--- Olympia/olympia/src/components/cadastro-user/CadastroUser.vue
+++ Olympia/olympia/src/components/cadastro-user/CadastroUser.vue
@@ -6,18 +6,16 @@
 
       <meu-dark-mode></meu-dark-mode>
 
-      <section class="signup ">
+      <section class="signup">
         <div class="container dark-register-div">
-          <div class="signup-content ">
+          <div class="signup-content">
             <div class="signup-form">
               <h2 class="form-title black-to-white">Cadastre-se</h2>
               <form class="register-form" id="register-form">
                 <div class="form-group">
-                  <label for="name"
-                    ><i
-                      class=" zmdi dark-register-ico zmdi-account material-icons-name"
-                    ></i
-                  ></label>
+                  <label for="name">
+                    <i class="zmdi dark-register-ico zmdi-account material-icons-name"></i>
+                  </label>
                   <input
                     class="dark-register-input"
                     type="text"
@@ -28,9 +26,9 @@
                   />
                 </div>
                 <div class="form-group">
-                  <label for="email"
-                    ><i class=" zmdi dark-register-ico zmdi-email"></i
-                  ></label>
+                  <label for="email">
+                    <i class="zmdi dark-register-ico zmdi-email"></i>
+                  </label>
                   <input
                     class="dark-register-input"
                     type="email"
@@ -41,9 +39,9 @@
                   />
                 </div>
                 <div class="form-group">
-                  <label for="tel"
-                    ><i class=" zmdi dark-register-ico zmdi-mail-send"></i
-                  ></label>
+                  <label for="tel">
+                    <i class="zmdi dark-register-ico zmdi-mail-send"></i>
+                  </label>
                   <input
                     class="dark-register-input"
                     type="text"
@@ -54,9 +52,9 @@
                   />
                 </div>
                 <div class="form-group">
-                  <label for="pass"
-                    ><i class=" zmdi dark-register-ico zmdi-lock"></i
-                  ></label>
+                  <label for="pass">
+                    <i class="zmdi dark-register-ico zmdi-lock"></i>
+                  </label>
                   <input
                     class="dark-register-input"
                     type="password"
@@ -67,9 +65,9 @@
                   />
                 </div>
                 <div class="form-group">
-                  <label for="re-pass"
-                    ><i class=" zmdi dark-register-ico zmdi-lock-outline"></i
-                  ></label>
+                  <label for="re-pass">
+                    <i class="zmdi dark-register-ico zmdi-lock-outline"></i>
+                  </label>
                   <input
                     class="dark-register-input"
                     type="password"
@@ -86,14 +84,12 @@
                     id="agree-term"
                     class="agree-term dark-register-input"
                   />
-                  <label
-                    for="agree-term"
-                    class="label-agree-term black-to-white"
-                    ><span><span></span></span>Eu concordo com os
-                    <a href="#" class="term-service"
-                      >Termos de Serviço</a
-                    ></label
-                  >
+                  <label for="agree-term" class="label-agree-term black-to-white">
+                    <span>
+                      <span></span>
+                    </span>Eu concordo com os
+                    <a href="#" class="term-service">Termos de Serviço</a>
+                  </label>
                 </div>
                 <div class="form-group form-button">
                   <input
@@ -103,13 +99,13 @@
                     class="form-submit"
                     value="Registrar"
                   />
-                </div>               
+                </div>
               </form>
             </div>
             <div class="form-group hexcolor">
-              <label for="hexcolor" style="margin-top:-13px"
-                ><i class=" zmdi dark-register-ico zmdi-palette"></i
-              ></label>
+              <label for="hexcolor" style="margin-top:-13px">
+                <i class="zmdi dark-register-ico zmdi-palette"></i>
+              </label>
               <input
                 type="text"
                 name="custom_color"
@@ -119,30 +115,37 @@
               />
             </div>
             <div class="color-holder call-picker" style="display: none"></div>
-            <div
-              class="color-picker"
-              id="color-picker"
-              style="display: none"
-            ></div>
+            <div class="color-picker" id="color-picker" style="display: none"></div>
 
             <div class="signup-image">
               <figure class="default-prof-user">
-                <img
-                  src="/src/assets/images/user-ico.png"
-                  alt="sing up image"
-                />
+                <img src="/src/assets/images/user-ico.png" alt="sing up image" id="user-prof-image" />
               </figure>
               <div id="user-let-img"></div>
-              <form class="image-form">
-                <div class="form-group form-button">
-                <input class="signup-image-link" type="file" name="image" accept="image/png, image/jpeg" value="Escolher foto de perfil">
-                </div>
-            </form>
-            </div> 
+              <p class="signup-image-link">Escolher Foto de Perfil</p>
+              <img class='bin' src='/src/assets/images/trash-can-icon.png'>
+            </div>
           </div>
         </div>
       </section>
-    
+      <div id="pick-image-modal">
+        <h3 class="file-input-title">Selecione uma Imagem</h3>
+        <ul class="file-list">
+          <li class="file-item file-item-1">
+            <div class="input-file-cont">
+              <input type="file" name="file" id="file" class="inputfile" accept=".jpg, .jpeg, .png" />
+              <label for="file" class="file-label">Choose a file</label>
+            </div>
+          </li>
+          <li class="file-item">
+            <div class="circle">
+              <img class="profile-pic" src="/src/assets/images/user-ico.png" />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="ui-widget-overlay"></div>
+
       <div id="background-wrap">
         <div class="bubble x1"></div>
         <div class="bubble x2"></div>
@@ -167,7 +170,6 @@
       </div>
       <a id="dark-mode-link">Modo Noturno</a>
     </div>
-    </div>
   </main>
 </template>
 
@@ -176,7 +178,7 @@ import DarkMode from "../shared/dark-mode/Dark-mode.vue";
 
 export default {
   components: {
-    "meu-dark-mode": DarkMode,
+    "meu-dark-mode": DarkMode
   }
 };
 </script>
