72a4a24d33ada11ba3e4ebd8d3dfb4dfa784cfd2
digo-smithh <smithrodrigues08@gmail.com>
2020-04-01 00:51:45 -0300

    edit

--- Olympia/olympia/src/components/shared/contatos/Contatos.vue
+++ Olympia/olympia/src/components/shared/contatos/Contatos.vue
@@ -7,27 +7,61 @@
                         <img class="close-2-img" src="/src/assets/images/x.png">
                     </div>
 
-                    <svg viewBox="0 0 500 250" x='699' y='999' version="1.1" xmlns="http://www.w3.org/2000/svg">     
+                    <svg class="svg-contact" viewBox="0 0 500 250" x='699' y='999' version="1.1" xmlns="http://www.w3.org/2000/svg">     
                         <circle id="contato-1" class="white-2" cx="100" cy="150" r="90" fill="white"></circle>
                         <circle id="contato-2" class="white-2" cx="210" cy="100" r="60" fill="white"></circle>
                         <circle id='contato-3' class="white-2" cx="315" cy="160" r="75" fill="white"></circle>
                     </svg>
                         <div class="div-text">
                             <div id="div-1">
-                                <p id='titulo'>C O N T A T O S</p>
-                                <p class='subtitulo text-gray'>DEP. ARTES PLÁSTICAS</p>
-                                <p class='texto text-gray-2'>Email: seuemail@celio.com<br>
-                                                                Telefone: sempaciencia</p>
+                                <div class="border-text">
+                                    <p id='titulo'>C O N T A T O S</p>
+                                    <p class='subtitulo text-gray'>DEP. ARTES PLÁSTICAS</p>
+                                    <p class='texto text-gray-2'>&#x1f582; artesplasticas@olympia.com<br>
+                                                                &#9743; 0800 123 0001</p>
+                                </div>
                             </div>
                             <div id="div-2">
-                                <p>Departamento artedc platicas emial:ddddddddddddddd tel :Dddddddddddd dpartamento
-                                    teatro emaisl : teeeeeeeeeeeeeeeeeeeeeeeee tel sddsdsd
-                                </p>
+                                <div class="border-text">
+                                    <p class='subtitulo text-gray'>DEP. ARQUITETURA</p>
+                                    <p class='texto text-gray-2'> &#x1f582; arquitetura@olympia.com<br>
+                                                                &#9743; 0800 123 0001</p>
+                                                              </div>
                             </div>
                             <div id="div-3">
-                                <p>Departamento artedc platicas emial:ddddddddddddddd tel :Dddddddddddd dpartamento
-                                    teatro emaisl : teeeeeeeeeeeeeeeeeeeeeeeee tel sddsdsd
-                                </p>
+                                <div class="border-text">
+                                    <p class='subtitulo text-gray'>DEP. LITERATURA</p>
+                                    <p class='texto text-gray-2'> &#x1f582; literatura@olympia.com<br>
+                                                                &#9743; 0800 123 0002</p>
+                                                              </div>
+                            </div>
+                            <div id="div-4">
+                                <div class="border-text">
+                                    <p class='subtitulo text-gray'>DEP. ARTES CÊNICAS</p>
+                                    <p class='texto text-gray-2'>&#x1f582; artescenicas@olympia.com<br>
+                                                                &#9743; 0800 123 0003</p>
+                                                             </div>
+                            </div>
+                            <div id="div-5">
+                                <div class="border-text">
+                                    <p class='subtitulo text-gray'>DEP. CINEMA</p>
+                                    <p class='texto text-gray-2'> &#x1f582; cinema@olympia.com<br>
+                                                                &#9743; 0800 123 0004</p>
+                                                              </div>
+                            </div>
+                            <div id="div-6">
+                                <div class="border-text">
+                                    <p class='subtitulo text-gray'>DEP. ESCULTURA</p>
+                                    <p class='texto text-gray-2'> &#x1f582; escultura@olympia.com<br>
+                                                                &#9743; 0800 123 0004</p>
+                                                              </div>
+                            </div>
+                            <div id="div-7">
+                                <div class="border-text">
+                                    <p class='subtitulo text-gray'>DEP. MÚSICA</p>
+                                    <p class='texto text-gray-2'> &#x1f582; musica@olympia.com<br>
+                                                                &#9743; 0800 123 0004</p>
+                                                              </div>
                             </div>
                         </div>
                 </div>
