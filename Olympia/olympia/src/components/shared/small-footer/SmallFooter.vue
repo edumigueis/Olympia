6e558dad6bd609669f5b72d504106ab3b53c3817
digo-smithh <smithrodrigues08@gmail.com>
2020-04-01 18:09:26 -0300

    edit

--- Olympia/olympia/src/components/shared/small-footer/SmallFooter.vue
+++ Olympia/olympia/src/components/shared/small-footer/SmallFooter.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="small-footer" class="white dark-mode-activators" style="margin-top: 50px;">
+    <a id="use-terms-smf" class="black-to-white">Termos de Uso</a>
     <div id="copyright-small-footer" class="wfont black-to-white">© OLYMPIA INC. All Rights Reserved.</div>
     <a id="dark-mode-link-smf" class="black-to-white">Modo Noturno</a>
   </div>
--- Olympia/olympia/src/components/welcome/Welcome.vue
+++ Olympia/olympia/src/components/welcome/Welcome.vue
@@ -1,8 +1,13 @@
 <template>
   <main>
     <div id="container-welcome">
-        <link rel="stylesheet" href="/src/assets/css/welcome.css">
-     
+      <title>W E L C O M E</title>
+      <link rel="stylesheet" href="/src/assets/css/welcome.css" />
+
+      <section class="canvas-wrap">
+        <div class="canvas-content"></div>
+        <div id="canvas" class="gradient"></div>
+      </section>
     </div>
   </main>
 </template>
