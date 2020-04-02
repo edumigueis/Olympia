7c91668815f40e9f91fb873969c09aa02605045b
digo-smithh <smithrodrigues08@gmail.com>
2020-04-01 23:13:23 -0300

    edit

--- Olympia/olympia/src/components/welcome/Welcome.vue
+++ Olympia/olympia/src/components/welcome/Welcome.vue
@@ -22,7 +22,7 @@
     <!-- TEXTS -->
     <mask maskUnits="userSpaceOnUse" x="-273" y="-353.2" width="3011.9" height="1453" id="bubbles-1">
         <g id="svg-texts">
-            <text transform="matrix(1 0 0 1 62.9893 351.1275)" style="fill:#FFFFFF;font-weight: 900; font-size:150px;">BEM VINDO AO OLYMPIA</text>
+            <text transform="matrix(1 0 0 1 62.9893 351.1275)" style="fill:#FFFFFF;font-weight: 900; font-size:180px;">BEM VINDO AO OLYMPIA</text>
         </g>
     </mask>
 
