84fb1062b5561a4b5138433b93edb28049a7d0ad
digo-smithh <smithrodrigues08@gmail.com>
2020-04-06 03:26:41 -0300

    edit

--- Olympia/olympia/src/assets/js/circle-menu/circleMenu.js
+++ Olympia/olympia/src/assets/js/circle-menu/circleMenu.js
@@ -1,7 +1,68 @@
-(function(window) {
-  'use strict';
+var jaFoi = false;
 
-  window.cssCircleMenu = function(el) {
+setInterval(() => {
+
+  if ($('.menu-container').length && jaFoi == false) {
+    jaFoi = true;
+
+    $(function () {
+
+      window.cssCircleMenu = function(el) {
+        var $menu = document.querySelector(el);
+        var $menuToggle = $menu ? $menu.querySelector('.js-menu-toggle') : undefined;
+        var $menuMask = $menu ? $menu.querySelector('.js-menu-mask') : undefined;
+    
+        if (!$menu || !$menuToggle || !$menuMask) {
+          throw new Error('Invalid elements, check the structure.');
+        } else {
+          init();
+        }
+    
+        return {
+          openMenu: openMenu,
+          closeMenu: closeMenu
+        };
+    
+        function init() {
+          $menuToggle.addEventListener('mouseenter', function(e) {
+            e.preventDefault();
+            toggleMenu();
+            if ($('.blur').css('filter') == 'none'){          
+              $('.blur').css('filter','blur(10px) !important');
+            }
+            else {
+              $('.blur').css('filter','none !important');
+              $('.menu-link-art').css('font-size','1.2em !important');
+            }
+          });
+        }
+    
+        function toggleMenu() {
+          $menuToggle.classList.contains('is-active')
+            ? closeMenu()
+            : openMenu();
+        }
+    
+        function openMenu() {
+          $menu.classList.add('is-active');
+          $menuToggle.classList.add('is-active');
+          $menuMask.classList.add('is-active');
+        }
+    
+        function closeMenu() {
+          $menu.classList.remove('is-active');
+          $menuToggle.classList.remove('is-active');
+          $menuMask.classList.remove('is-active');
+        }
+      };
+    })
+  }
+  else if (!$('.menu-container').length) {
+    jaFoi = false;
+  }
+}, 100);
+
+window.cssCircleMenu = function(el) {
     var $menu = document.querySelector(el);
     var $menuToggle = $menu ? $menu.querySelector('.js-menu-toggle') : undefined;
     var $menuMask = $menu ? $menu.querySelector('.js-menu-mask') : undefined;
@@ -51,4 +112,4 @@
       $menuMask.classList.remove('is-active');
     }
   };
-})(window);
+
