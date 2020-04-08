3549a59d298ce9d28df050f8869cc02428567ff4
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-04-08 17:47:48 -0300

    Finalização da página de evento

--- Olympia/olympia/src/assets/js/event.js
+++ Olympia/olympia/src/assets/js/event.js
@@ -2,24 +2,24 @@
 //VARIÁVEIS AUXILIARES
 var jaFoi = false;
 
-setInterval(() => {  //REASTREADOR, VERIFICA TUDO A TODO MOMENTO
+setInterval(() => {
+  //REASTREADOR, VERIFICA TUDO A TODO MOMENTO
 
   if ($("#container-evento").length && jaFoi == false) {
-
     jaFoi = true;
 
-    $(function () {
-        $('.view-gallery button').on('click', function() {
-            $('.box').toggleClass('animated');
-            // $('ul').css({
-            //   'max-width' : '100%'
-            // });
-          });
-        
+    $(function() {
+      $(".view-gallery button").on("click", function() {
+        $(".box").toggleClass("animated");
+        // $('ul').css({
+        //   'max-width' : '100%'
+        // });
+      });
+
+     
     });
-
-
-  } else if (!$("#container-evento").length) { //SE SAIU DA ROTA, TUDO VOLTA PARA O NORMAL
+  } else if (!$("#container-evento").length) {
+    //SE SAIU DA ROTA, TUDO VOLTA PARA O NORMAL
     jaFoi = false;
   }
 }, 100);
--- Olympia/olympia/src/components/evento/Evento.vue
+++ Olympia/olympia/src/components/evento/Evento.vue
@@ -73,7 +73,8 @@
             </div>
             <p class="about-event-text-in">
               <span>
-                Lorem ipsum dolor sit amet, <a class="underlined underlined--reverse">consectetur adipiscing elit</a>. Nunc tincidunt velit nulla, id pharetra ex pellentesque et. Ut at ante malesuada neque vulputate fringilla a eget leo. Nam ultrices diam eu metus tristique, vel faucibus lacus sollicitudin. Aliquam metus urna, elementum eu finibus eu, hendrerit maximus nunc. Sed id massa et nibh scelerisque semper in at magna. Suspendisse sed ultrices felis. Praesent felis leo, elementum id maximus eget, blandit vitae eros. Proin vel lacus lacus. Maecenas mauris purus, finibus a efficitur sed, lobortis sit amet augue. Morbi elementum massa at pulvinar tempus. Nullam suscipit nisl eu erat faucibus pretium. Aliquam sodales eget dolor vestibulum facilisis. Praesent consectetur, justo vitae molestie ullamcorper, nisl sapien iaculis mi, sit amet accumsan lorem felis non tellus.
+                Lorem ipsum dolor sit amet,
+                <a class="underlined underlined--reverse">consectetur adipiscing elit</a>. Nunc tincidunt velit nulla, id pharetra ex pellentesque et. Ut at ante malesuada neque vulputate fringilla a eget leo. Nam ultrices diam eu metus tristique, vel faucibus lacus sollicitudin. Aliquam metus urna, elementum eu finibus eu, hendrerit maximus nunc. Sed id massa et nibh scelerisque semper in at magna. Suspendisse sed ultrices felis. Praesent felis leo, elementum id maximus eget, blandit vitae eros. Proin vel lacus lacus. Maecenas mauris purus, finibus a efficitur sed, lobortis sit amet augue. Morbi elementum massa at pulvinar tempus. Nullam suscipit nisl eu erat faucibus pretium. Aliquam sodales eget dolor vestibulum facilisis. Praesent consectetur, justo vitae molestie ullamcorper, nisl sapien iaculis mi, sit amet accumsan lorem felis non tellus.
                 Praesent vitae mollis sapien. Sed id neque sem. Aenean purus nunc, fermentum varius risus ut, porttitor blandit tellus. Sed fermentum consectetur lectus, nec iaculis nisi molestie eget. Vestibulum rutrum tincidunt arcu, sed malesuada nisi. Curabitur libero urna, pulvinar quis faucibus sit amet, venenatis nec odio. Ut vel nibh vehicula, porttitor mi nec, laoreet justo. Duis euismod pharetra leo, et ultrices lacus placerat at. Vestibulum lorem erat, euismod ac dolor luctus, pharetra pretium eros. Nulla sit amet consequat arcu, et aliquam ante. Integer iaculis, turpis at venenatis tincidunt, dolor purus tristique massa, id tincidunt ante lorem porttitor purus. Duis felis sapien, ullamcorper sit amet suscipit eu, tristique id nulla. Duis tristique dolor non consectetur ultrices. Curabitur eget libero lobortis, posuere erat sed, rhoncus mauris.
                 Nullam vel quam felis. Donec egestas interdum enim, quis hendrerit sem consequat vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed dictum posuere orci in consectetur. Proin vitae lacus euismod, blandit magna id, sollicitudin tortor. Vivamus at fermentum neque. Fusce a rutrum metus. Duis porta sollicitudin dui. In lobortis laoreet laoreet. Quisque ut mi at nisi scelerisque viverra non in eros. Integer aliquam ipsum nec elit mattis, a commodo sem porta.
               </span>
@@ -88,20 +89,54 @@
               <div>LOCALIZAÇÃO</div>
             </div>
             <div id="bottom-location">
-            <div class="map-container">
-                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.026733576006!2d-73.97981028509388!3d40.761436742527366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258f97bdb102b%3A0xea9f8fc0b3ffff55!2sThe%20Museum%20of%20Modern%20Art!5e0!3m2!1spt-BR!2sbr!4v1586369480301!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+              <div class="map-container">
+                <iframe
+                  class="map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.026733576006!2d-73.97981028509388!3d40.761436742527366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258f97bdb102b%3A0xea9f8fc0b3ffff55!2sThe%20Museum%20of%20Modern%20Art!5e0!3m2!1spt-BR!2sbr!4v1586369480301!5m2!1spt-BR!2sbr"
+                  frameborder="0"
+                  style="border:0;"
+                  allowfullscreen
+                  aria-hidden="false"
+                  tabindex="0"
+                ></iframe>
+              </div>
+              <img src="/src/assets/images/finish.png" class="location-icon" />
+              <p id="y-r-here">Você está aqui!</p>
+              <div class="address-cont">
+                <p class="popout">
+                  <span>11 W 53rd St,</span>
+                  <br />
+                  <span>New York, NY,</span>
+                  <br />
+                  <span>10019, Estados Unidos</span>
+                </p>
+              </div>
             </div>
-            <img src="/src/assets/images/finish.png" class="location-icon">
-            <p id="y-r-here">Você está aqui!</p>
-            <div class="address-cont">
-                <p class="about-event-loc-in">Endereço: 11 W 53rd St, New York, NY 10019, Estados Unidos</p>
-            </div>
-            </div>
-            
           </div>
         </div>
       </div>
-
+      <div id="background-wrap">
+        <div class="bubble x1"></div>
+        <div class="bubble x2"></div>
+        <div class="bubble x3"></div>
+        <div class="bubble x4"></div>
+        <div class="bubble x5"></div>
+        <div class="bubble x6"></div>
+        <div class="bubble x7"></div>
+        <div class="bubble x8"></div>
+        <div class="bubble x9"></div>
+        <div class="bubble x10"></div>
+        <div class="bubble x11"></div>
+        <div class="bubble x12"></div>
+        <div class="bubble x13"></div>
+        <div class="bubble x14"></div>
+        <div class="bubble x15"></div>
+        <div class="bubble x16"></div>
+        <div class="bubble x17"></div>
+        <div class="bubble x18"></div>
+        <div class="bubble x19"></div>
+        <div class="bubble x20"></div>
+      </div>
       <meu-footer></meu-footer>
     </div>
   </main>
