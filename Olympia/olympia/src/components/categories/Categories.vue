91ee1c6a87f4af3df0a396099b97ef392ad2acb7
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-04-13 10:56:37 -0300

    Criada a estrutura básica da página de categorias

---
+++ Olympia/olympia/src/components/categories/Categories.vue
@@ -0,0 +1,40 @@
+<template>
+  <main style="overflow:hidden;">
+    <div id="container-categorias">
+      <link rel="stylesheet" href="/src/assets/css/detalhe.css" />
+
+      <meu-menu-bar></meu-menu-bar>
+      <meu-menu-items></meu-menu-items>
+      <meus-contatos></meus-contatos>
+      <meu-dark-mode></meu-dark-mode>
+      <meu-mouse></meu-mouse>
+
+
+
+
+
+
+
+      <meu-footer></meu-footer>
+    </div>
+  </main>
+</template>
+<script>
+import MenuBar from "../shared/menu-bar/Menu-bar.vue";
+import MenuItems from "../shared/menu-items/Menu-items.vue";
+import Contatos from "../shared/contatos/Contatos.vue";
+import Mouse from "../shared/mouse/Mouse.vue";
+import Footer from "../shared/footer/Footer.vue";
+import DarkMode from "../shared/dark-mode/Dark-mode.vue";
+
+export default {
+  components: {
+    "meu-menu-bar": MenuBar,
+    "meu-menu-items": MenuItems,
+    "meus-contatos": Contatos,
+    "meu-mouse": Mouse,
+    "meu-footer": Footer,
+    "meu-dark-mode": DarkMode
+  }
+};
+</script>
