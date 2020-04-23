34b0eeefbfff67041ec1ce35d998df434879890e
Manuela Vicente Benassi <manuelavbenassi@gmail.com>
2020-04-23 10:56:01 -0300

    adição de imagens na pagina de literatura

--- Olympia/olympia/src/components/literatura/Literatura.vue
+++ Olympia/olympia/src/components/literatura/Literatura.vue
@@ -4,7 +4,7 @@
       <title>L I T E R A T U R A</title>
       <link rel="stylesheet" href="/src/assets/css/event-detail.css" />
       <link rel="shortcut icon" href="/src/assets/images/page-ico.png" />
-       <link rel="stylesheet" href="/src/assets/css/literatura.css" />
+      <link rel="stylesheet" href="/src/assets/css/literatura.css" />
 
       <meu-menu-bar></meu-menu-bar>
       <meu-menu-items></meu-menu-items>
@@ -13,46 +13,164 @@
       <meu-mouse></meu-mouse>
 
       <!--Seu código aqui-->
-    <div style="position:relative">
-      <div class="image-container">
-                <img
-                  src="https://images.unsplash.com/photo-1465433045946-ba6506ce5a59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-                  alt
-                  class="image"
-                />
-              </div>
+      <div style="position:relative">
+        <div class="image-container">
+          <img
+            src="https://images.unsplash.com/photo-1465433045946-ba6506ce5a59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+            alt class="image" />
+        </div>
+        <div class="bg"></div>
+        <!--BACKGROUND-->
 
+
+        <div id="corpo">
+          <div class="container-left">
+            <table>
+              <!--TABELA PERMETE UMA BOA RESPONSIVIDADE PELO VERTICAL-ALIGN NO CSS-->
+              <tr>
+                <td class="texto-principal">
+                  <h1 class="text-title-principal">Literatura</h1>
+                  A Literatura é a técnica de compor e expor textos escritos, em prosa ou em verso, de acordo com
+                  princípios teóricos e práticos; o exercício dessa técnica ou da eloquência e poesia.
+
+                  A palavra Literatura vem do latim "litteris" que significa "Letras", e possivelmente uma tradução do
+                  grego "grammatikee". Em latim, literatura significa uma instrução ou um conjunto de saberes ou
+                  habilidades de escrever e ler bem, e se relaciona com as técnicas da gramática, da retórica e da
+                  poética. Por extensão, se refere especificamente à arte ou ofício de escrever. O termo Literatura
+                  também é usado como referência a um conjunto escolhido de textos como, por exemplo a literatura
+                  portuguesa, a literatura espanhola, a literatura inglesa, a literatura brasileira, a literatura
+                  japonesa, etc.</p>Cinema (do em grego: κίνημα, kinema "movimentos" e γράφειν,
+                  graphein "registrar"), também chamada sétima arte, ou, em certos
+                  contextos cinematografia, pode ser definida como a técnica e a
+                  arte de fixar e de reproduzir imagens que suscitam impressão de
+                  movimento, assim como a indústria que produz estas imagens. As
+                  obras cinematográficas (mais conhecidas como filmes) são
+                  produzidas através da gravação de imagens do mundo com câmeras
+                  (câmaras) adequadas, ou pela sua criação utilizando técnicas de
+                  animação ou efeitos visuais específicos. Mais especificamente,
+                  pode ser descrita como o "conjunto de princípios, processos e
+                  técnicas utilizados para captar e projetar numa tela imagens
+                  estáticas sequenciais (fotogramas) obtidas com uma câmera
+                  especial, dando impressão ao espectador de estarem em
+                  movimento". O diretor de arte pode ser descrito como o principal
+                  colaborador visual de um diretor de cinema.Também se usa a
+                  palavra 'cinegrafia', estando dicionarizada.
+                </td>
+              </tr>
+            </table>
+          </div>
+
+          <!--<img class="linha" src="https://img.pngio.com/black-line-png-just-pink-about-it-black-line-png-900_150.png"/>-->
+
+          <div class="container-right">
+            <table>
+              <tr>
+                <td class="texto-linha">
+                  <h1 class="text-title">O cortiço</h1>
+                  O cenário do romance de Aluísio Azevedo é o cortiço São Romão, situado no Rio de Janeiro, durante o
+                  século XIX.
+                  O proprietário do estabelecimento é João Romão, um português que se muda para o Brasil em busca de uma
+                  vida melhor e consegue
+                  montar seu próprio estabelecimento.
+                  No princípio o proprietário tinha apenas três casas, depois consegue comprar casas ao lado e aos
+                  poucos vai levantando novas
+                  habitações.
+                  João Romão tem como companheira Bertoleza, uma escrava fugida. Querendo ampliar cada vez mais os
+                  negócios,
+                  o português faz uma sociedade com o vizinho Miranda, e, para selar a união, propõe o casamento com
+                  Zulmira,
+                  a filha do sócio.
+                  Sem saber o que fazer com a sua companheira Bertoleza, João Romão pretende denunciá-la como escrava
+                  fugida.
+                  O romance de Aluísio Azevedo narra, com pormenores, o cotidiano miserável daqueles que vivem no
+                  cortiço.
+                </td>
+                <td class="td-imagem">
+                  <img class="imagem" margin-right:100px src="https://cdn.culturagenial.com/imagens/o-cortic3a7o.jpg" />
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div class="container-left">
+            <table>
+              <tr>
+                <td class="texto-linha">
+                  <h1 class="text-title">Dom casmurro</h1>
+                  A pergunta que até hoje perdura na literatura brasileira permanece sem resposta: Capitu traiu ou não
+                  traiu Bentinho?
+                  O clássico Dom Casmurro, de Machado de Assis, conta a história de um triângulo amoroso composto pelo
+                  narrador Bento Santiago,
+                  pela sua esposa Capitu e pelo melhor amigo do narrador, Escobar.
+                  Um ciumento inveterado, Bentinho via nos gestos da esposa possíveis sinais de que ela estaria tendo um
+                  caso com o seu amigo
+                  de infância. Mesmo após a morte do amigo, Bentinho segue assombrado pela desconfiança. Ainda no
+                  velório ele interpreta o olhar
+                  de Capitu em direção ao morto como um olhar apaixonado.
+                  A suspeita da traição ganha mais força quando nasce Ezequiel, filho do casal, um bebê que o narrador
+                  afirma carregar os traços
+                  do melhor amigo, e não os seus.
+                </td>
+                <td class="td-imagem">
+                  <img class="imagem" margin-right:100px
+                    src="https://cdn.culturagenial.com/imagens/dom-casmurro-djvu-cke.jpg" />
+                </td>
+              </tr>
+            </table>
+          </div>
+          <div class="container-right">
+            <table>
+              <tr>
+                <td class="texto-linha">
+                  <h1 class="text-title">Triste fim de Policarpo Quaresma</h1>
+                  Policarpo Quaresma é o protagonista do romance de Lima Barreto passado no Rio de Janeiro no final do
+                  século XIX.
+                  Considerado uma obra pré-modernista, o livro conta a história de um patriota ufanista que faz de tudo
+                  para louvar o que é nacional.
+                  Policarpo consegue alcançar o cargo de subsecretário do Arsenal de Guerra e se radicaliza cada vez
+                  mais em nome da sua paixão:
+                  passa a comer apenas comidas típicas brasileiras, aprende modinhas nacionais no violão e decide se
+                  comunicar em tupi-guarani.
+                  O extremismo começa a trazer problemas e Policarpo muda-se com a irmã para o campo. A mudança, no
+                  entanto,
+                  não faz com que os conflitos desapareçam, as desavenças entre os vizinhos no interior trazem a tona
+                  novas questões.
+                </td>
+                <td class="td-imagem">
+                  <img class="imagem" margin-right:100px
+                    src="https://images-na.ssl-images-amazon.com/images/I/91F8jky4-KL.jpg" />
+                </td>
+              </tr>
+            </table>
+          </div>
+
+
+
+        </div>
+      </div>
+      <!---->
     </div>
-     <h1 class="title">Literatura</h1>
-     <div class="caixa-texto">
-       <p class="texto">  A Literatura é a técnica de compor e expor textos escritos, em prosa ou em verso, de acordo com princípios teóricos e práticos; o exercício dessa técnica ou da eloquência e poesia.
+    <meu-footer></meu-footer>
 
-A palavra Literatura vem do latim "litteris" que significa "Letras", e possivelmente uma tradução do grego "grammatikee". Em latim, literatura significa uma instrução ou um conjunto de saberes ou habilidades de escrever e ler bem, e se relaciona com as técnicas da gramática, da retórica e da poética. Por extensão, se refere especificamente à arte ou ofício de escrever. O termo Literatura também é usado como referência a um conjunto escolhido de textos como, por exemplo a literatura portuguesa, a literatura espanhola, a literatura inglesa, a literatura brasileira, a literatura japonesa, etc.</p>
-  
-     </div>
-    <!---->
-
-      <meu-footer></meu-footer>
-    </div>
   </main>
 </template>
 
 <script>
-import MenuBar from "../shared/menu-bar/Menu-bar.vue";
-import MenuItems from "../shared/menu-items/Menu-items.vue";
-import Contatos from "../shared/contatos/Contatos.vue";
-import Mouse from "../shared/mouse/Mouse.vue";
-import Footer from "../shared/footer/Footer.vue";
-import DarkMode from "../shared/dark-mode/Dark-mode.vue";
+  import MenuBar from "../shared/menu-bar/Menu-bar.vue";
+  import MenuItems from "../shared/menu-items/Menu-items.vue";
+  import Contatos from "../shared/contatos/Contatos.vue";
+  import Mouse from "../shared/mouse/Mouse.vue";
+  import Footer from "../shared/footer/Footer.vue";
+  import DarkMode from "../shared/dark-mode/Dark-mode.vue";
 
-export default {
-  components: {
-    "meu-menu-bar": MenuBar,
-    "meu-menu-items": MenuItems,
-    "meus-contatos": Contatos,
-    "meu-mouse": Mouse,
-    "meu-footer": Footer,
-    "meu-dark-mode": DarkMode
-  }
-};
+  export default {
+    components: {
+      "meu-menu-bar": MenuBar,
+      "meu-menu-items": MenuItems,
+      "meus-contatos": Contatos,
+      "meu-mouse": Mouse,
+      "meu-footer": Footer,
+      "meu-dark-mode": DarkMode
+    }
+  };
+
 </script>
