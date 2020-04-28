d6c0cc44814a2dd94e903813afa727ba9ef2af50
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-04-28 14:13:26 -0300

    Adição do slider de quotes na cinema

---
+++ Olympia/src/assets/js/cinema.js
@@ -0,0 +1,42 @@
+var jaFoiCin = false;
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+setInterval(() => {  //REASTREADOR, VERIFICA TUDO A TODO MOMENTO
+
+  if ($("#container-categorias").length && jaFoiCin == false) {
+
+    jaFoiCin = true;
+
+    $(function () {
+        
+      
+    })
+  }
+  else if (!$("#container-categorias").length) { //SE SAIU DA ROTA, TUDO VOLTA PARA O NORMAL
+    jaFoiCin = false;
+  }
+}, 100);
--- Olympia/src/components/cinema/Cinema.vue
+++ Olympia/src/components/cinema/Cinema.vue
@@ -2,7 +2,6 @@
   <main>
     <div id="container-cinema">
       <title>C I N E M A</title>
-      <link rel="stylesheet" href="/src/assets/css/event-detail.css" />
       <link rel="shortcut icon" href="/src/assets/images/page-ico.png" />
       <link rel="stylesheet" href="/src/assets/css/cinema.css" />
 
@@ -27,6 +26,33 @@
       </div>
 
       <div id="corpo">
+        <div class="slideshow-container">
+
+<div class="mySlides">
+  <q>I love you the more in that I believe you had liked me for my own sake and for nothing else</q>
+  <p class="author">- John Keats</p>
+</div>
+
+<div class="mySlides">
+  <q>But man is not made for defeat. A man can be destroyed but not defeated.</q>
+  <p class="author">- Ernest Hemingway</p>
+</div>
+
+<div class="mySlides">
+  <q>I have not failed. I've just found 10,000 ways that won't work.</q>
+  <p class="author">- Thomas A. Edison</p>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+
+<div class="dot-container">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
         <div class="container-left">
           <table> <!--TABELA PERMETE UMA BOA RESPONSIVIDADE PELO VERTICAL-ALIGN NO CSS-->
             <tr>
