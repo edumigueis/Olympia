7d6fd0cb92c0a65f797d4113dd84d1c1d8882999
digo-smithh <smithrodrigues08@gmail.com>
2020-05-04 02:45:19 -0300

    Continuação da pagina de perfil

--- olympia/src/assets/js/perfil.js
+++ olympia/src/assets/js/perfil.js
@@ -26,8 +26,65 @@ setInterval(() => {
             $('#footer').css('display', 'block');
         });
 
+        $(document.body).on('click', '.estatisticas', function () {
+            $('.sobre-mim-content').fadeOut(300);
+            $('.curtidas-content').fadeOut(300);
+            $('.salvos-content').fadeOut(300);
+            $('.configuracoes-content').fadeOut(300);
+            $('.estatisticas-content').fadeIn(300);
+        });
+
+        $(document.body).on('click', '.sobre-mim', function () {
+            $('.sobre-mim-content').fadeIn(300);
+            $('.curtidas-content').fadeOut(300);
+            $('.salvos-content').fadeOut(300);
+            $('.configuracoes-content').fadeOut(300);
+            $('.estatisticas-content').fadeOut(300);
+        });
+
+        $(document.body).on('click', '.curtidas', function () {
+            $('.sobre-mim-content').fadeOut(300);
+            $('.curtidas-content').fadeIn(300);
+            $('.salvos-content').fadeOut(300);
+            $('.configuracoes-content').fadeOut(300);
+            $('.estatisticas-content').fadeOut(300);
+        });
+
+        $(document.body).on('click', '.salvos', function () {
+            $('.sobre-mim-content').fadeOut(300);
+            $('.curtidas-content').fadeOut(300);
+            $('.salvos-content').fadeIn(300);
+            $('.configuracoes-content').fadeOut(300);
+            $('.estatisticas-content').fadeOut(300);
+        });
+
+        $(document.body).on('click', '.configuracoes', function () {
+            $('.sobre-mim-content').fadeOut(300);
+            $('.curtidas-content').fadeOut(300);
+            $('.salvos-content').fadeOut(300);
+            $('.configuracoes-content').fadeIn(300);
+            $('.estatisticas-content').fadeOut(300);
+        });
+
+        $(document.body).on('click', '.colorful', function () {
+            $('.bg').addClass('colorful-css');
+            $('.bg').removeClass('black-and-white-css');
+            $('.bg').removeClass('black-css');
+        });
+
+        $(document.body).on('click', '.black-and-white', function () {
+            $('.bg').addClass('black-and-white-css');
+            $('.bg').removeClass('colorful-css');
+            $('.bg').removeClass('black-css');
+        });
+
+        $(document.body).on('click', '.black', function () {
+            $('.bg').addClass('black-css');
+            $('.bg').removeClass('colorful-css');
+            $('.bg').removeClass('black-and-white-css');
+        });
     }
     else if (!$('#container-perfil').length) {
         jaFoiPerfil = false;
     }
-}, 100);
+}, 300);
--- olympia/src/components/perfil/Perfil.vue
+++ olympia/src/components/perfil/Perfil.vue
@@ -3,6 +3,7 @@
     <div id="container-perfil">
       <title>P E R F I L</title>
       <link rel="shortcut icon" href="/src/assets/images/page-ico.png" />
+      <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
       <link rel="stylesheet" href="/src/assets/css/perfil.css" />
 
       <meu-menu-bar></meu-menu-bar>
@@ -24,86 +25,121 @@
                 class="img-profile"
               />
             </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
-            </div>
-            <div class="user-pic">
-              <img
-                src="/src/assets/images/perfil.jpeg"
-                alt
-                class="img-profile"
-              />
+            <div class="info-user">
+              <div class="info-1">
+                <h1 class="name-user">José Marques Costa</h1>
+                <h2 class="user">@jose.costa</h2>
+                <div class="info-estatisticas">
+                  <p class="seguindo">1.2k seguindo</p>
+                  <p class="seguidores">53.9k seguidores</p>
+                  <p class="publicacoes">13 publicações</p>
+                </div>
+              </div>
+              <div class="info-2">
+                <a class="link-ico"
+                  ><img src="/src/assets/images/edit.png" class="img-ico"
+                /></a>
+              </div>
             </div>
           </div>
+          <main ontouchstart class="tab-bar">
+            <nav class="tab-link">
+              <a class="tab-link-a estatisticas">Estatísticas</a>
+              <a class="tab-link-a sobre-mim">Sobre mim</a>
+              <a class="tab-link-a curtidas">Curtidas</a>
+              <a class="tab-link-a salvos">Salvos</a>
+              <a class="tab-link-a configuracoes">Configurações</a>
+              <hr />
+            </nav>
+          </main>
         </div>
-        <div class="background-perfil"></div>
+        <div class="container-wrap">
+          <div>
+            <svg
+              class="waves"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              viewBox="0 24 150 28"
+              preserveAspectRatio="none"
+              shape-rendering="auto"
+            >
+              <defs>
+                <path
+                  id="gentle-wave"
+                  d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                />
+              </defs>
+              <g class="parallax">
+                <use
+                  xlink:href="#gentle-wave"
+                  x="48"
+                  y="0"
+                  fill="rgba(255,255,255,0.7"
+                />
+                <use
+                  xlink:href="#gentle-wave"
+                  x="48"
+                  y="3"
+                  fill="rgba(255,255,255,0.5)"
+                />
+                <use
+                  xlink:href="#gentle-wave"
+                  x="48"
+                  y="5"
+                  fill="rgba(255,255,255,0.3)"
+                />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+              </g>
+            </svg>
+          </div>
+        </div>
+        <div class="perfil-content">
+          <div class="estatisticas-content content-perfil">
+            <hr />
+          </div>
+          <div class="sobre-mim-content content-perfil">
+            <hr />
+          </div>
+          <div class="curtidas-content content-perfil">
+            <hr />
+          </div>
+          <div class="salvos-content content-perfil">
+            <hr />
+          </div>
+          <div class="configuracoes-content content-perfil">
+            <hr />
+            <h1 class="capas-config">Efeito da capa...</h1>
+            <section class="config-section">
+              <div class="div-capa">
+                <input type="radio" id="control_01" name="select" value="1" checked/>
+                <label for="control_01" class="label-capa colorful" >
+                  <h2>COLORFUL</h2>
+                  <p>
+                    Que tal uma animação bem colorida para adoçar seu dia??
+                  </p>
+                </label>
+              </div>
+              <div class="div-capa">
+                <input type="radio" id="control_02" name="select" value="2" />
+                <label class="label-capa black-and-white" for="control_02">
+                  <h2>B&W</h2>
+                  <p>
+                    Ou, então, em preto e branco?
+                  </p>
+                </label>
+              </div>
+              <div class="div-capa">
+                <input type="radio" id="control_03" name="select" value="3" />
+                <label class="label-capa black" for="control_03">
+                  <h2>BLACK</h2>
+                  <p>
+                    Tá, então vamos no pretinho básico.
+                  </p>
+                </label>
+              </div>
+            </section>
+          </div>
+        </div>
       </div>
       <div id="myModal" class="modal">
         <span class="close-mod">&times;</span>
