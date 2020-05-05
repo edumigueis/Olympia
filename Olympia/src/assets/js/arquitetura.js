856dfbc2ca85bc95084fbc4af250c7b66f9cee70
Manuela Vicente Benassi <manuelavbenassi@gmail.com>
2020-05-05 15:01:38 -0300

    criação da pagina de arquitetura

---
+++ Olympia/src/assets/js/arquitetura.js
@@ -0,0 +1,41 @@
+
+var slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+var jaFoiArq = false;
+setInterval(() => {  //REASTREADOR, VERIFICA TUDO A TODO MOMENTO
+  if ($("#container-cinema").length && jaFoiArq == false) {
+
+    jaFoiArq = true;
+
+    $(function () {
+      showSlides(slideIndex);
+    })
+  }
+  else if (!$("#container-cinema").length) { //SE SAIU DA ROTA, TUDO VOLTA PARA O NORMAL
+    jaFoiArq = false;
+  }
+}, 100);
