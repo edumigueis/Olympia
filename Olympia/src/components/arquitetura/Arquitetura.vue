f9c46b58f39504355f44fb1ff840478541f02068
Manuela Vicente Benassi <manuelavbenassi@gmail.com>
2020-05-06 12:03:44 -0300

    ajustes de conteúdo da pagina de arquitetura

--- Olympia/src/components/arquitetura/Arquitetura.vue
+++ Olympia/src/components/arquitetura/Arquitetura.vue
@@ -21,25 +21,26 @@
           />
         </div>
         <h1 class="art-title">Arquitetura</h1>
-        <h2 class="art-subtitle">A sétima arte, um mar de sensações</h2>
+        <h2 class="art-subtitle">A quarta arte, um mar de sensações</h2>
       </div>
 
       <div id="corpo">
         <div class="slideshow-container dark-slideshow">
 
       <div class="mySlides">
-        <q>"O arquitetura não tem fronteiras nem limites. É um fluxo constante de sonho."</q>
-        <p class="author">- Orson Welles</p>
+        <q>"A arquitetura é o jogo sábio, correto e magnífico dos volumes dispostos sob a luz."</q>
+        <p class="author">- Le Corbusier</p>
       </div>
 
       <div class="mySlides">
-        <q>"Para o arquitetura tudo se torna uma imensa natureza-morta, até os sentimentos dos outros são qualquer coisa de que se pode dispor."</q>
-        <p class="author">- Federico Fellini</p>
+        <q>"A felicidade é como encontrar o belo na arquitetura: não esta onde procuramos e só encontramos quando somos humildes o suficente pra entender que a simplicidade é o caminho."</q>
+        <p class="author">- Sú Franke.</p>
       </div>
 
       <div class="mySlides">
-        <q>"O cinema não é um ofício. É uma arte. Cinema não é um trabalho de equipe. O diretor está só diante de uma página em branco."</q>
-        <p class="author">- Jean-Luc Godard
+        <q>"A arquitetura é o engenho na insônia do poeta."</q>
+        <p class="author">- Brenon Salvador
+    
     </p>
       </div>
 
@@ -57,23 +58,22 @@
           <table> <!--TABELA PERMETE UMA BOA RESPONSIVIDADE PELO VERTICAL-ALIGN NO CSS-->
             <tr>
               <td class="texto-principal">
-                <h1 class="text-title-principal">CINEMA</h1>
-                Cinema (do em grego: κίνημα, kinema "movimentos" e γράφειν,
-                graphein "registrar"), também chamada sétima arte, ou, em certos
-                contextos cinematografia, pode ser definida como a técnica e a
-                arte de fixar e de reproduzir imagens que suscitam impressão de
-                movimento, assim como a indústria que produz estas imagens. As
-                obras cinematográficas (mais conhecidas como filmes) são
-                produzidas através da gravação de imagens do mundo com câmeras
-                (câmaras) adequadas, ou pela sua criação utilizando técnicas de
-                animação ou efeitos visuais específicos. Mais especificamente,
-                pode ser descrita como o "conjunto de princípios, processos e
-                técnicas utilizados para captar e projetar numa tela imagens
-                estáticas sequenciais (fotogramas) obtidas com uma câmera
-                especial, dando impressão ao espectador de estarem em
-                movimento". O diretor de arte pode ser descrito como o principal
-                colaborador visual de um diretor de cinema.Também se usa a
-                palavra 'cinegrafia', estando dicionarizada.
+                <h1 class="text-title-principal">Arquitetura</h1>
+               Arquitetura refere-se a toda construção e modelagem artificial do ambiente físico, incluindo seu processo
+               de projeto e o produto deste, sendo a palavra também usada para definir os estilos e métodos de projeto das
+               construções de uma época. Em outras áreas, como, por exemplo, ciência da computação, o termo arquitetura se
+               refere à estrutura geral de um sistema, sendo como um sinônimo de algo que projetado ou a forma como 
+               funciona.O interior do Panteão, obra icônica da arquitetura romana que inovou nas técnicas construtivas
+               e resistiu ao tempo.O prédio da Fundação Iberê Camargo, projetado por Álvaro Siza, é um exemplo de 
+               arquitetura contemporânea.A principal aplicação da palavra se refere ao projeto de edifícios pela 
+               humanidade, uma área de atuação multidisciplinar que trabalha entre arte e ciência e também engloba 
+               o projeto de paisagens, cidades, interiores, móveis e objetos individuais - áreas como paisagismo, 
+               urbanismo, design industrial e planejamento regional estão, assim, diretamente relacionados com a 
+               arquitetura, sendo muitas vezes ensinados junto com esta em escolas. Desde o Renascimento, a arquitetura 
+               é considerada uma das artes plásticas. Sociologia e filosofia também estão presentes, com tendências como 
+               racionalismo, construtivismo e fenomenologia sendo parte importante da história e das teorias da arquitetura.
+              O trabalho do profissional, chamado arquiteto ou arquiteta, envolve, portanto, toda a escala da vida humana, 
+              desde a manual até a urbana.
               </td>
             </tr>
           </table>
@@ -85,25 +85,18 @@
           <table>
             <tr>
               <td class="texto-linha">
-                <h1 class="text-title">HISTÓRIA</h1>
-                Louis e Auguste eram filhos e colaboradores do industrial
-                Antoine Lumière, fotógrafo e fabricante de películas
-                fotográficas, proprietário da Fábrica Lumière (Usine Lumière),
-                instalada na cidade francesa de Lyon. Antoine reformou-se em
-                1892, deixando a fábrica entregue aos filhos. O cinematógrafo
-                era uma máquina de filmar e projetor de cinema, invento que lhes
-                tem sido atribuído mas que na verdade foi inventado por Léon
-                Bouly no ano de 1892, o qual teria perdido o registro dessa
-                patente, sendo então de novo registrado pelos irmãos Lumiere em
-                13 de fevereiro de 1895. São considerados os inventores da
-                Sétima Arte junto com Georges Méliès, também francês, sendo este
-                visto como o pai do cinema de ficção.
+                <h1 class="text-title"> Museu Guggenheim de Nova York</h1>
+                O Guggenheim se trata de uma das grandes obras de arquitetura, um exemplar valioso do movimento neo-expressionista, 
+                surgido nos anos oitenta.
+                Ele foi projetado por Frank Lloyd Wright, um pouco antes de sua morte.
+                Internamente, apresenta um amplo e fluido espaço.
+                É repleto de rampas em espiral e inundado por luz natural, proveniente de uma belíssima cúpula na cobertura do edifício.
               </td>
               <td class="td-imagem">
                 <img
                   class="imagem"
                   margin-right:100px
-                  src="https://upload.wikimedia.org/wikipedia/commons/9/93/Fratelli_Lumiere.jpg"
+                  src="https://imagens-revista-pro.vivadecora.com.br/uploads/2018/05/obras-de-arquitetura-famosas-guggenheim-nova-iorque-768x493.jpg"
                 />
               </td>
             </tr>
@@ -114,14 +107,17 @@
           <table> <!--TABELA PERMETE UMA BOA RESPONSIVIDADE PELO VERTICAL-ALIGN NO CSS-->
             <tr>
               <td class="texto-linha">
-                <h1 class="text-title">E o vento levou</h1>
-                A complicada vida de Scarlett O’Hara, seus amores e desilusões em um período que tem a Guerra Civil Americana como pano de fundo. Rhett Butler é um vivido aventureiro que passa pela vida dela, em uma relação de amor e ódio marcada por conflitos já clássicos e cenas inesquecíveis de amor. Praticamente o inventor das telenovelas, devido aos conflitos constantes de emoções manifestadas e o romance como tema – não necessariamente por uma outra pessoa, e sim por uma causa, lugar ou qualquer outra coisa que se refira sentimentalmente ao personagem.
+                <h1 class="text-title">Museu Guggenheim de Bilbao</h1>
+               Outro Museu Guggenheim que está na lista das obras de arquitetura famosas é o de Bilbao, na Espanha.
+               O edifício escultural projetado por Frank Gehry possui formas que parecem desafiar a gravidade e a lógica.
+               As folhas brilhantes de metal aplicadas em sua fachada remetem a uma grande embarcação ancorada no Rio Nervión.
+               Seu volume desconstrutivista se destaca na paisagem e encanta os espectadores.
               </td>
               <td class="td-imagem">
                 <img
                   class="imagem"
                   margin-right:100px
-                  src="https://cinemalivre.net/imagens/capas/e_o_vento_levou_1939_o.jpg"
+                  src="https://imagens-revista-pro.vivadecora.com.br/uploads/2018/05/obras-de-arquitetura-famosas-guggenheim-bilbao-768x462.jpg"
                 />
               </td>
             </tr>
@@ -131,14 +127,17 @@
           <table>
             <tr>
               <td class="texto-linha">
-                <h1 class="text-title">Assim caminha a humanidade</h1>
-                Depois que um rancheiro texano aparece para comprar um cavalo premiado em uma fazenda distante, ele acaba se apaixonando pela filha do proprietário, com quem logo se casa. Uma vez de volta aos Texas, sua nova família enfrenta um cowboy rival a ele, em um confronto que se estende por duas gerações.
+                <h1 class="text-title">Ópera de Sydney</h1>
+                A Ópera de Sydney também é considerada como uma das obras de arquitetura famosas.
+                Seu design foi bastante inovador para o século XX.
+                Projeto de Jorn Utzon, o edifício é composto de três conchas interligadas que servem de cobertura para as salas de espetáculos, além de outras instalações.
+                Depois de mais de quarenta anos de funcionamento, essa grande obra se tornou símbolo da nação australiana.
               </td>
               <td class="td-imagem">
                 <img
                   class="imagem"
                   margin-right:100px
-                  src="https://br.web.img2.acsta.net/pictures/14/02/06/13/55/182482.jpg"
+                  src="https://imagens-revista-pro.vivadecora.com.br/uploads/2018/05/obras-de-arquitetura-famosas-opera-de-sydney.jpg"
                 />
               </td>
             </tr>
@@ -148,24 +147,18 @@
           <table> 
             <tr>
               <td class="texto-linha">
-                <h1 class="text-title">Magnólia</h1>
-                A história se desenvolve em Los Angeles, nos arredores da rua Magnólia, acompanhando um dia na vida de 
-                nove personagens, que moram na mesma área e cujas histórias se cruzam por coincidências do destino. 
-                O filme aborda diversos temas polêmicos como vício, suicídio, abuso infantil, e homossexualidade.
-                Big Earl Partridge (Jason Robards) é um produtor de TV à beira da morte que deseja reencontrar o filho.
-                Phill (Phillip Seymour Hoffman) é seu dedicado enfermeiro e Linda (Juliane Moore), sua jovem esposa. 
-                Frank (Tom Cruise), um célebre guru machista, é o filho que Big Earl procura. 
-                Jimmy Gator (Philip Baker Hall) é o apresentador de um famoso programa de TV que também está com câncer 
-                e procurando se entender com a filha Cláudia (Melora Walters), cocainomaníaca. Stanley (Jeremy Blackman) 
-                é um garoto-prodígio manipulado pelo pai oportunista.  
-                Mas, antes que este círculo de personagens se feche, o espectador ainda terá muitas surpresas...
-                Magnólia foi elogiado por suas atuações, direção, ambição, narrativa de histórias, edição e trilha sonora. Recebeu três indicações ao Oscar 2000 incluindo Melhor Roteiro Original, Melhor Ator Coadjuvante para Tom Cruise e melhor canção original.              
+                <h1 class="text-title">Templo de Lótus, Nova Deli</h1>
+               Agora, de todas as construções famosas desta lista, a Lótus indiana poder ser, de longe, uma das mais belas.
+               O templo foi projetado pelo arquiteto Fariborz Sahba.
+               Trata-se de um templo erguido para uma religião monoteísta chamada de Fé bahá’í, que prega a união espiritual.
+               A ampla sala central possui nove acessos.
+               Eles simbolizam, justamente, a possibilidade da junção harmoniosa de pessoas de diferentes crenças por um mesmo propósito: o da paz.           
               </td>
               <td class="td-imagem">
                 <img
                   class="imagem"
                   margin-right:100px
-                  src="https://www.cafecomfilme.com.br/media/k2/items/cache/9a9975ed61463a6dc929492ab2b7763b_XL.jpg?t=20170320_031632"
+                  src="https://imagens-revista-pro.vivadecora.com.br/uploads/2018/05/obras-de-arquitetura-famosas-templo-de-lotus.jpg"
                 />
               </td>
             </tr>
@@ -175,20 +168,91 @@
           <table>
             <tr>
               <td class="texto-linha">
-                <h1 class="text-title">O poderoso chefão</h1>
-                Amplamente considerado um dos melhores filmes de todos os tempos, esse drama da máfia, baseado no romance homônimo de Mario Puzo, concentra-se na poderosa família criminosa ítalo-americana de Don Vito Corleone (Marlon Brando). Quando o filho mais novo de Don, Michael (Al Pacino), se junta com relutância à Máfia, ele se envolve no inevitável ciclo de violência e traição. Embora Michael tente manter um relacionamento normal com sua esposa, Kay (Diane Keaton), ele está mais envolvido nos negócios da família.
+                <h1 class="text-title">Museu do Amanhã</h1>
+                Concebido como um dos ícones arquitetônicos do Porto Maravilha, projeto de requalificação da região 
+                portuária do Rio de Janeiro, 
+                o Museu do Amanhã ocupa 15 mil metros quadrados.
+                Sua forma longilínea, inspirada nas bromélias do Jardim Botânico, foi projetada de maneira a se integrar
+                à paisagem ao redor e, especialmente, deixar visível o Mosteiro de São Bento, um dos mais importantes 
+                conjuntos barrocos do país. 
               </td>
               <td class="td-imagem">
                 <img
                   class="imagem"
                   margin-right:100px
-                  src="https://br.web.img3.acsta.net/medias/nmedia/18/90/93/20/20120876.jpg"
+                  src="https://museudoamanha.org.br/sites/default/files/Calatrava_1280x800_MdA-ByronPrujansky.jpg"
                 />
               </td>
             </tr>
           </table>
         </div>  
+        <div class="container-left black-to-white white-7">
+          <table> 
+            <tr>
+              <td class="texto-linha">
+                <h1 class="text-title">MASP</h1>
+               O edifício do Museu de Arte de São Paulo (Masp) é de autoria da arquiteta ítalo-brasileira Lina Bo Bardi 
+               (1914-1992). Localizado na Avenida Paulista, em São Paulo, o projeto da sede tem início em 1957 e, 
+               inaugurado mais de uma década depois, em 7 de novembro de 1968. Ocupa o terreno do antigo Clube Belvedere
+              Trianon (1916), local de encontro da elite paulistana e sede da 1ª Bienal de Arte de São Paulo (1951). 
+              Sua demolição ocorre em 1953 para dar lugar a uma edificação permanente destinada à arte.
+              </td>
+              <td class="td-imagem">
+                <img
+                  class="imagem"
+                  margin-right:100px
+                  src="https://upload.wikimedia.org/wikipedia/commons/9/9d/MASP_Brazil.jpg"
+                />
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div class="container-right black-to-white white-7">
+          <table>
+            <tr>
+              <td class="texto-linha">
+                <h1 class="text-title">Museu de Arte Contemporânea de Niterói</h1>
+               Um único cilindro surge numa ponta da Baía da Guanabara. Dele desabrocha em flor o edifício branco, circular, 
+               embelezando a natureza verde. Um espelho d’água dá continuidade visual ao mar. E o museu que de longe parecia 
+               brotar da terra, surge em verdade do mar.
+               A forma circular do museu, com 16 metros de altura, juntamente com grandes vãos, resultou em uma solução estrutural
+              essencialmente radial, dividida em seis setores. A grande rampa de concreto exterior leva os visitantes, através 
+              de 98 metros de espaço livre, até as entradas aos andares superiores.
+              </td>
+              <td class="td-imagem">
+                <img
+                  class="imagem"
+                  margin-right:100px
+                  src="https://i.ytimg.com/vi/yj7fXHgUTrc/maxresdefault.jpg"
+                />
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div class="container-left black-to-white white-7">
+          <table> 
+            <tr>
+              <td class="texto-linha">
+                <h1 class="text-title">Ópera de Arame, Curitiba</h1>
+              A região onde está situada a Ópera de Arame era, de fato, uma pedreira, que pertencia à família Gava. A região, que estava bastante encoberta pela vegetação, foi descoberta por um assessor.
+
+Assim que visitaram o espaço, surgiu na equipe a ideia de projetar um edifício transparente, que fosse capaz de trazer o lado de fora para dentro, como se a decoração das paredes fosse a própria natureza ao redor.
+Com uma estrutura circular, o objetivo maior era que o edifício não entrasse em conflito com a paisagem.
+
+O projeto do edifício ganhou corpo durante o mês de setembro de 1991. A construção da Ópera de Arame foi feita a partir de vidros, estruturas metálicas e tubos de aço.
+              </td>
+              <td class="td-imagem">
+                <img
+                  class="imagem"
+                  margin-right:100px
+                  src="https://cdn.culturagenial.com/imagens/curitiba-opera-de-arame3-cke.jpg"
+                />
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
+
       <!---->
 
       <meu-footer></meu-footer>
