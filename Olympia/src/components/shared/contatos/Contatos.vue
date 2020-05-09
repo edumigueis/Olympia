310956833056d86aed365b5f1ae9d1fe015ba4a6
digo-smithh <smithrodrigues08@gmail.com>
2020-05-08 23:29:11 -0300

    Atualização dos email de contato

--- Olympia/src/components/shared/contatos/Contatos.vue
+++ Olympia/src/components/shared/contatos/Contatos.vue
@@ -1,71 +1,116 @@
 <template>
-    <table class="table-contato">
-        <tr>
-            <td>
-                <div id='container-contatos'>
-                    <div class="close-2 close">
-                        <img class="close-2-img close-image" src="/src/assets/images/x.png">
-                    </div>
-                        <svg class="svg-contact" viewBox="0 0 500 250" x='699' y='999' version="1.1" xmlns="http://www.w3.org/2000/svg">     
-                            <circle id="contato-1" class="white-2" cx="100" cy="150" r="90" fill="white"></circle>
-                            <circle id="contato-2" class="white-2" cx="210" cy="100" r="60" fill="white"></circle>
-                            <circle id='contato-3' class="white-2" cx="315" cy="160" r="75" fill="white"></circle>
-                        </svg>
-                        <div class="div-text">
-                            <div id="div-1" class="div-content">
-                                <div class="border-text">
-                                    <p id='titulo'>C O N T A T O S</p>
-                                    <p class='subtitulo text-gray'>DEP. ARTES PLÁSTICAS</p>
-                                    <p class='texto text-gray-2'>&#x1f582; artesplasticas@olympia.com<br>
-                                                                &#9743; 0800 123 0001</p>
-                                </div>
-                            </div>
-                            <div id="div-2" class="div-content">
-                                <div class="border-text">
-                                    <p class='subtitulo text-gray'>DEP. ARQUITETURA</p>
-                                    <p class='texto text-gray-2'> &#x1f582; arquitetura@olympia.com<br>
-                                                                &#9743; 0800 123 0001</p>
-                                                              </div>
-                            </div>
-                            <div id="div-3" class="div-content"> 
-                                <div class="border-text">
-                                    <p class='subtitulo text-gray'>DEP. LITERATURA</p>
-                                    <p class='texto text-gray-2'> &#x1f582; literatura@olympia.com<br>
-                                                                &#9743; 0800 123 0002</p>
-                                                              </div>
-                            </div>
-                            <div id="div-4" class="div-content">
-                                <div class="border-text">
-                                    <p class='subtitulo text-gray'>DEP. ARTES CÊNICAS</p>
-                                    <p class='texto text-gray-2'>&#x1f582; artescenicas@olympia.com<br>
-                                                                &#9743; 0800 123 0003</p>
-                                                             </div>
-                            </div>
-                            <div id="div-5" class="div-content">
-                                <div class="border-text">
-                                    <p class='subtitulo text-gray'>DEP. CINEMA</p>
-                                    <p class='texto text-gray-2'> &#x1f582; cinema@olympia.com<br>
-                                                                &#9743; 0800 123 0004</p>
-                                                              </div>
-                            </div>
-                            <div id="div-6" class="div-content">
-                                <div class="border-text">
-                                    <p class='subtitulo text-gray'>DEP. ESCULTURA</p>
-                                    <p class='texto text-gray-2'> &#x1f582; escultura@olympia.com<br>
-                                                                &#9743; 0800 123 0004</p>
-                                                              </div>
-                            </div>
-                            <div id="div-7" class="div-content">
-                                <div class="border-text">
-                                    <p class='subtitulo text-gray'>DEP. MÚSICA</p>
-                                    <p class='texto text-gray-2'> &#x1f582; musica@olympia.com<br>
-                                                                &#9743; 0800 123 0004</p>
-                                                              </div>
-                            </div>
-                        </div>
-                </div>
-            </td>
-        </tr>
-    </table>
+  <table class="table-contato">
+    <tr>
+      <td>
+        <div id="container-contatos">
+          <div class="close-2 close">
+            <img
+              class="close-2-img close-image"
+              src="/src/assets/images/x.png"
+            />
+          </div>
+          <svg
+            class="svg-contact"
+            viewBox="0 0 500 250"
+            x="699"
+            y="999"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              id="contato-1"
+              class="white-2"
+              cx="100"
+              cy="150"
+              r="90"
+              fill="white"
+            ></circle>
+            <circle
+              id="contato-2"
+              class="white-2"
+              cx="210"
+              cy="100"
+              r="60"
+              fill="white"
+            ></circle>
+            <circle
+              id="contato-3"
+              class="white-2"
+              cx="315"
+              cy="160"
+              r="75"
+              fill="white"
+            ></circle>
+          </svg>
+          <div class="div-text">
+            <div id="div-1" class="div-content">
+              <div class="border-text">
+                <p id="titulo">C O N T A T O S</p>
+                <p class="subtitulo text-gray">PRECISA DE AJUDA?</p>
+                <p class="texto text-gray-2">
+                  mande sua dúvida no email <br />
+                  ajuda-olympia@gmail.com
+                </p>
+              </div>
+            </div>
+            <div id="div-2" class="div-content">
+              <div class="border-text">
+                <p class="subtitulo text-gray">OCORREU ALGUM ERRO?</p>
+                <p class="texto text-gray-2">
+                  nos detalhe, com prints, no email <br />
+                  erros-olympia@gmail.com
+                </p>
+              </div>
+            </div>
+            <div id="div-3" class="div-content">
+              <div class="border-text">
+                <p class="subtitulo text-gray">TEM SUGESTÕES?</p>
+                <p class="texto text-gray-2">
+                  estamos de ouvidos abertos no email <br />
+                  sugestoes-olympia@gmail.com
+                </p>
+              </div>
+            </div>
+            <div id="div-4" class="div-content">
+              <div class="border-text">
+                <p class="subtitulo text-gray">ALGUMA DENÚNCIA?</p>
+                <p class="texto text-gray-2">
+                  pode nos contar no email <br />
+                  denuncias-olympia@gmail.com
+                </p>
+              </div>
+            </div>
+            <div id="div-5" class="div-content">
+              <div class="border-text">
+                <p class="subtitulo text-gray">QUE TAL UMA PARCERIA?</p>
+                <p class="texto text-gray-2">
+                  mande sua proposta no email <br />
+                  parceria-olympia@gmail.com
+                </p>
+              </div>
+            </div>
+            <div id="div-6" class="div-content">
+              <div class="border-text">
+                <p class="subtitulo text-gray">QUER TRABALHAR CONOSCO?</p>
+                <p class="texto text-gray-2">
+                  vamos conversar no email <br />
+                  work-olympia@gmail.com
+                </p>
+              </div>
+            </div>
+            <div id="div-7" class="div-content">
+              <div class="border-text">
+                <p class="subtitulo text-gray">CRÉDITOS</p>
+                <p class="texto text-gray-2">
+                  Eduardo Migueis <br />
+                  Manuela Benassi<br />
+                  Rodrigo Smith<br />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </td>
+    </tr>
+  </table>
 </template>
-
