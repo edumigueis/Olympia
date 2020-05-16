4abcf6fe02278a1a68217ceee558ceaa0b0243dc
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-05-16 16:27:18 -0300

    Correções

--- Olympia/src/assets/js/welcome-animation/3d-lines-animation.js
+++ Olympia/src/assets/js/welcome-animation/3d-lines-animation.js
@@ -122,7 +122,6 @@ function onDocumentTouchMove(event) {
 
     if (event.touches.length == 1) {
 
-        event.preventDefault();
 
         mouseX = event.touches[0].pageX - windowHalfX;
         mouseY = event.touches[0].pageY - windowHalfY;
