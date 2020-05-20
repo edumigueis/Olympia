163c3ee75604d54e929bbbc66e1b59e83d8a68b8
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-05-19 23:31:49 -0300

    Início do Formulário  de FeedBack

--- Olympia/src/assets/js/feedback.js
+++ Olympia/src/assets/js/feedback.js
@@ -1,10 +1,43 @@
-
+// The scroll-down function
+function scrollDown() {
+    var vheight = $(window).height();
+    $("html, body").animate(
+      {
+        scrollTop: (Math.floor($(window).scrollTop() / vheight) + 1) * vheight
+      },
+      700
+    );
+  }
+  function scrollDownPlus() {
+    var vheight = $(window).height();
+    $("html, body").animate(
+      {
+        scrollTop:
+          (Math.floor($(window).scrollTop() / vheight) + 1) * vheight + 300
+      },
+      700
+    );
+  }
 var jaFoiFB = false;
 setInterval(() => {  //REASTREADOR, VERIFICA TUDO A TODO MOMENTO
   if ($("#container-feedback").length && jaFoiFB == false) {
 
     jaFoiFB = true;
-    
+    $('.module').on('click', function(){
+        $(".wrapper-form").css("display", "block");
+        
+        var width = $(window).width();
+        if (width < 901) {
+          scrollDownPlus();
+        } else {
+          scrollDown();
+        }
+        event.preventDefault();
+        setTimeout(function() {
+          $(".choose-wrapper").css("display", "none");
+          $("svg").css("display", "block");
+        }, 840);
+    })
   }
   else if (!$("#container-feedback").length) { //SE SAIU DA ROTA, TUDO VOLTA PARA O NORMAL
     jaFoiFB = false;
--- Olympia/src/components/feedback/FeedBack.vue
+++ Olympia/src/components/feedback/FeedBack.vue
@@ -9,109 +9,202 @@
       <meu-menu-items></meu-menu-items>
       <meus-contatos></meus-contatos>
       <meu-dark-mode></meu-dark-mode>
-      <h1>Onde está o problema?</h1>
-      <div class="wrapper-sec">
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://image.flaticon.com/icons/svg/25/25694.svg"
-              />
+
+      <section class="choose-wrapper">
+        <h1>Onde está o problema?</h1>
+        <div class="wrapper-sec">
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img src="https://image.flaticon.com/icons/svg/25/25694.svg" />
+              </div>
             </div>
+            <div class="flex-part"><h2 class="title">Feed</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
           </div>
-          <div class="flex-part"><h2 class="title">Feed</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img
+                  src="https://i.pinimg.com/originals/ff/77/56/ff7756247e080009b8418d80e47c5c91.png"
+                />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Daily Contents</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img
+                  src="https://image.flaticon.com/icons/png/512/82/82582.png"
+                />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Perfil</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img
+                  src="https://image.flaticon.com/icons/png/512/16/16036.png"
+                />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Login</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img
+                  src="https://static.thenounproject.com/png/521193-200.png"
+                />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Conteúdos</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img src="https://img.icons8.com/all/500/settings.png" />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Configurações</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img src="/src/assets/images/plus-sign.png" />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Postar</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img
+                  src="https://static.thenounproject.com/png/17840-200.png"
+                />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Eventos</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
+          <div class="module">
+            <div class="flex-part">
+              <div class="img-wp">
+                <img
+                  src="https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png"
+                />
+              </div>
+            </div>
+            <div class="flex-part"><h2 class="title">Outro</h2></div>
+            <div class="flex-part"><div class="arrow"></div></div>
+          </div>
         </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://i.pinimg.com/originals/ff/77/56/ff7756247e080009b8418d80e47c5c91.png"
-              />
-            </div>
+      </section>
+      <div class="wrapper-form">
+        <svg
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          width="800px"
+          height="600px"
+          viewBox="0 0 800 600"
+          enable-background="new 0 0 800 600"
+          xml:space="preserve"
+        >
+          <linearGradient
+            id="SVGID_1_"
+            gradientUnits="userSpaceOnUse"
+            x1="174.7899"
+            y1="186.34"
+            x2="330.1259"
+            y2="186.34"
+            gradientTransform="matrix(0.8538 0.5206 -0.5206 0.8538 147.9521 -79.1468)"
+          >
+            <stop offset="0" style="stop-color:#FFC035" />
+            <stop offset="0.221" style="stop-color:#F9A639" />
+            <stop offset="1" style="stop-color:#E64F48" />
+          </linearGradient>
+          <circle fill="url(#SVGID_1_)" cx="266.498" cy="211.378" r="77.668" />
+          <linearGradient
+            id="SVGID_2_"
+            gradientUnits="userSpaceOnUse"
+            x1="290.551"
+            y1="282.9592"
+            x2="485.449"
+            y2="282.9592"
+          >
+            <stop offset="0" style="stop-color:#FFA33A" />
+            <stop offset="0.0992" style="stop-color:#E4A544" />
+            <stop offset="0.9624" style="stop-color:#00B59C" />
+          </linearGradient>
+          <circle fill="url(#SVGID_2_)" cx="388" cy="282.959" r="97.449" />
+          <linearGradient
+            id="SVGID_3_"
+            gradientUnits="userSpaceOnUse"
+            x1="180.3469"
+            y1="362.2723"
+            x2="249.7487"
+            y2="362.2723"
+          >
+            <stop offset="0" style="stop-color:#12B3D6" />
+            <stop offset="1" style="stop-color:#7853A8" />
+          </linearGradient>
+          <circle fill="url(#SVGID_3_)" cx="215.048" cy="362.272" r="34.701" />
+          <linearGradient
+            id="SVGID_4_"
+            gradientUnits="userSpaceOnUse"
+            x1="367.3469"
+            y1="375.3673"
+            x2="596.9388"
+            y2="375.3673"
+          >
+            <stop offset="0" style="stop-color:#12B3D6" />
+            <stop offset="1" style="stop-color:#7853A8" />
+          </linearGradient>
+          <circle fill="url(#SVGID_4_)" cx="482.143" cy="375.367" r="114.796" />
+          <linearGradient
+            id="SVGID_5_"
+            gradientUnits="userSpaceOnUse"
+            x1="365.4405"
+            y1="172.8044"
+            x2="492.4478"
+            y2="172.8044"
+            gradientTransform="matrix(0.8954 0.4453 -0.4453 0.8954 127.9825 -160.7537)"
+          >
+            <stop offset="0" style="stop-color:#FFA33A" />
+            <stop offset="1" style="stop-color:#DF3D8E" />
+          </linearGradient>
+          <circle fill="url(#SVGID_5_)" cx="435.095" cy="184.986" r="63.504" />
+        </svg>
+
+        <div class="container">
+          <h2 class="title-form">Detalhe o Problema:</h2>
+          <form>
+            <textarea class="email" placeholder="email" />
+            <br />
+            <label class="file-lb" for='selecao-arquivo'>Selecionar um arquivo &#187;</label>
+            <input type="file" id="selecao-arquivo"/>
+          </form>
+          <br />
+          <div class="btn-cont">
+              <button class="register">
+            <span>register</span>
+          </button>
           </div>
-          <div class="flex-part"><h2 class="title">Daily Contents</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://image.flaticon.com/icons/png/512/82/82582.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Perfil</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://image.flaticon.com/icons/png/512/16/16036.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Login</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://static.thenounproject.com/png/521193-200.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Conteúdos</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://img.icons8.com/all/500/settings.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Configurações</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="/src/assets/images/plus-sign.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Postar</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://static.thenounproject.com/png/17840-200.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Eventos</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
-        </div>
-        <div class="module">
-          <div class="flex-part">
-            <div class="img-wp">
-              <img
-                src="https://cdn4.iconfinder.com/data/icons/pictype-free-vector-icons/16/more-512.png"
-              />
-            </div>
-          </div>
-          <div class="flex-part"><h2 class="title">Outro</h2></div>
-          <div class="flex-part"><div class="arrow"></div></div>
+          
         </div>
       </div>
-      <div class="wrapper-form"></div>
 
       <meu-footer></meu-footer>
     </div>
