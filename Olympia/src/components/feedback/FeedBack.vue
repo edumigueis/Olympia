72ce62a9c53fbeb88a19cb5fb87bac18a46f656b
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-05-20 12:52:00 -0300

    Continuação da Página de FeedBack

--- Olympia/src/components/feedback/FeedBack.vue
+++ Olympia/src/components/feedback/FeedBack.vue
@@ -4,6 +4,15 @@
       <title>F E E D B A C K</title>
       <link rel="shortcut icon" href="/src/assets/images/page-ico.png" />
       <link rel="stylesheet" href="/src/assets/css/feedback.css" />
+      
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+      />
 
       <meu-menu-bar></meu-menu-bar>
       <meu-menu-items></meu-menu-items>
@@ -112,97 +121,29 @@
         <svg
           version="1.1"
           id="Layer_1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          width="800px"
-          height="600px"
-          viewBox="0 0 800 600"
-          enable-background="new 0 0 800 600"
           xml:space="preserve"
         >
-          <linearGradient
-            id="SVGID_1_"
-            gradientUnits="userSpaceOnUse"
-            x1="174.7899"
-            y1="186.34"
-            x2="330.1259"
-            y2="186.34"
-            gradientTransform="matrix(0.8538 0.5206 -0.5206 0.8538 147.9521 -79.1468)"
-          >
-            <stop offset="0" style="stop-color:#FFC035" />
-            <stop offset="0.221" style="stop-color:#F9A639" />
-            <stop offset="1" style="stop-color:#E64F48" />
-          </linearGradient>
-          <circle fill="url(#SVGID_1_)" cx="266.498" cy="211.378" r="77.668" />
-          <linearGradient
-            id="SVGID_2_"
-            gradientUnits="userSpaceOnUse"
-            x1="290.551"
-            y1="282.9592"
-            x2="485.449"
-            y2="282.9592"
-          >
-            <stop offset="0" style="stop-color:#FFA33A" />
-            <stop offset="0.0992" style="stop-color:#E4A544" />
-            <stop offset="0.9624" style="stop-color:#00B59C" />
-          </linearGradient>
-          <circle fill="url(#SVGID_2_)" cx="388" cy="282.959" r="97.449" />
-          <linearGradient
-            id="SVGID_3_"
-            gradientUnits="userSpaceOnUse"
-            x1="180.3469"
-            y1="362.2723"
-            x2="249.7487"
-            y2="362.2723"
-          >
-            <stop offset="0" style="stop-color:#12B3D6" />
-            <stop offset="1" style="stop-color:#7853A8" />
-          </linearGradient>
-          <circle fill="url(#SVGID_3_)" cx="215.048" cy="362.272" r="34.701" />
-          <linearGradient
-            id="SVGID_4_"
-            gradientUnits="userSpaceOnUse"
-            x1="367.3469"
-            y1="375.3673"
-            x2="596.9388"
-            y2="375.3673"
-          >
-            <stop offset="0" style="stop-color:#12B3D6" />
-            <stop offset="1" style="stop-color:#7853A8" />
-          </linearGradient>
-          <circle fill="url(#SVGID_4_)" cx="482.143" cy="375.367" r="114.796" />
-          <linearGradient
-            id="SVGID_5_"
-            gradientUnits="userSpaceOnUse"
-            x1="365.4405"
-            y1="172.8044"
-            x2="492.4478"
-            y2="172.8044"
-            gradientTransform="matrix(0.8954 0.4453 -0.4453 0.8954 127.9825 -160.7537)"
-          >
-            <stop offset="0" style="stop-color:#FFA33A" />
-            <stop offset="1" style="stop-color:#DF3D8E" />
-          </linearGradient>
-          <circle fill="url(#SVGID_5_)" cx="435.095" cy="184.986" r="63.504" />
+          <circle class="shape" />
+          <circle class="shape" />
+          <circle class="shape" />
+          <circle class="shape" />
+          <circle class="shape" />
         </svg>
-
+        <div class="frm-wrapper">
         <div class="container">
           <h2 class="title-form">Detalhe o Problema:</h2>
           <form>
-            <textarea class="email" placeholder="email" />
+            <textarea class="email" placeholder="Detalhes..." />
             <br />
-            <label class="file-lb" for='selecao-arquivo'>Selecionar um arquivo &#187;</label>
+            <label class="file-lb" for='selecao-arquivo'><i class="fa fa-paperclip"></i></label>
             <input type="file" id="selecao-arquivo"/>
           </form>
           <br />
           <div class="btn-cont">
-              <button class="register">
-            <span>register</span>
-          </button>
+              <input type="submit" class="register" value="Registrar">
           </div>
           
+        </div>
         </div>
       </div>
 
