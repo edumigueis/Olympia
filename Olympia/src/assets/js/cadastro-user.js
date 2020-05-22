ea72c642f9888caba0e443f77ad490e84dbba649
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-05-22 20:15:03 -0300

    Added fail treatment

--- Olympia/src/assets/js/cadastro-user.js
+++ Olympia/src/assets/js/cadastro-user.js
@@ -484,6 +484,7 @@ setInterval(() => {
           data: jsonInput,
           contentType:'application/json',
           success: function(){ document.location.href = "/#/login"},
+          fail: function(){ $(".error-modal").css("display", "block");$(".error-modal").css("opacity", "1");$(".ui-widget-overlay").css("display", "block");$(".warn").html("Algúm dado foi fornecido incorretamente. Ou ocorreu um problema com o servidor.");$("#name").val("");setTimeout(function(){location.reload();}, 2000)},
           dataType: "json"
         });
         }
