74ab405c4ecb3507f23a798c3f2c0c178c81664c
digo-smithh <smithrodrigues08@gmail.com>
2020-05-23 21:23:26 -0300

    edit

--- API-olympia/Startup.cs
+++ API-olympia/Startup.cs
@@ -5,13 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using API_olympia.Data;
 
 namespace API_olympia
 {
@@ -27,22 +23,7 @@ namespace API_olympia
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy(name: "myPolicy",
-                                    builder =>
-                                    {
-                                        builder.AllowAnyOrigin()
-                                               .AllowAnyMethod()
-                                               .AllowAnyHeader();
-                                    });
-            });
-
-            services.AddDbContext<OlympiaContext>(
-                x => x.UseSqlServer(Configuration.GetConnectionString("StringConexaoSQLServer"))
-            );
-            services.AddControllers();
-            services.AddScoped<IRepository, Repository>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,18 +33,24 @@ namespace API_olympia
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseCors("myPolicy");
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
