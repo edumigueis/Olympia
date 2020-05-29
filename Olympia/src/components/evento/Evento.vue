9d73069ee481f7e458dc442ee7983b303cbe50a3
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-05-29 18:52:59 -0300

    Formatting

--- Olympia/src/components/evento/Evento.vue
+++ Olympia/src/components/evento/Evento.vue
@@ -3,7 +3,10 @@
     <div id="container-evento">
       <link rel="stylesheet" href="/src/assets/css/event-detail.css" />
       <link rel="shortcut icon" href="/src/assets/images/page-ico.png" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
       <title>E V E N T O</title>
       <meu-menu-bar></meu-menu-bar>
       <meu-menu-items></meu-menu-items>
@@ -19,7 +22,6 @@
           </div>
         </div>
         <div id="bottom-header" class="white-6-special">
-
           <div id="calendar-red">
             <p class="month">SET</p>
             <div id="calendar-white">
@@ -27,24 +29,45 @@
             </div>
           </div>
           <div id="title">
-            <p id="event-name" class="black-to-white">Mirrors by Fenjhki MoMA</p>
+            <p id="event-name" class="black-to-white">
+              Mirrors by Fenjhki MoMA
+            </p>
           </div>
-        </div>                     
+        </div>
       </div>
       <div id="page-rol">
         <div class="grid-item gray" id="grid-item-1">
           <div id="event-info">
             <div class="event-info-p gray">
-              <p id="event-info-nome" class="event-info-item black-to-white">Mirrors by Fenjhki MoMA</p>
-              <p id="event-info-local" class="event-info-item black-to-white">
-                Endereço: 11 W 53rd St, New York, NY 10019,
-                Estados Unidos
+              <p id="event-info-nome" class="event-info-item black-to-white">
+                Mirrors by Fenjhki MoMA
               </p>
-              <p id="event-info-datas" class="event-info-item black-to-white">Datas: De 23/09/2020 a 27/10/2020</p>
-              <p id="event-info-horarios" class="event-info-item black-to-white">Horários: 9:00-16:00</p>
-              <p id="event-info-precominimo" class="event-info-item black-to-white">Preço mínimo: USD $20</p>
+              <p id="event-info-local" class="event-info-item black-to-white">
+                Endereço: 11 W 53rd St, New York, NY 10019, Estados Unidos
+              </p>
+              <p id="event-info-datas" class="event-info-item black-to-white">
+                Datas: De 23/09/2020 a 27/10/2020
+              </p>
+              <p
+                id="event-info-horarios"
+                class="event-info-item black-to-white"
+              >
+                Horários: 9:00-16:00
+              </p>
+              <p
+                id="event-info-precominimo"
+                class="event-info-item black-to-white"
+              >
+                Preço mínimo: USD $20
+              </p>
               <p id="event-info-website" class="event-info-item">
-                <a href="https://www.moma.org" target="_blank" id="of-web-link-eve" class="tickets-link black-to-white">Compre Ingressos</a>
+                <a
+                  href="https://www.moma.org"
+                  target="_blank"
+                  id="of-web-link-eve"
+                  class="tickets-link black-to-white"
+                  >Compre Ingressos</a
+                >
               </p>
             </div>
           </div>
@@ -75,9 +98,43 @@
             <p class="about-event-text-in">
               <span id="desc-ev-wrapper">
                 Lorem ipsum dolor sit amet,
-                <a class="underlined underlined--reverse black-to-white">consectetur adipiscing elit</a>. Nunc tincidunt velit nulla, id pharetra ex pellentesque et. Ut at ante malesuada neque vulputate fringilla a eget leo. Nam ultrices diam eu metus tristique, vel faucibus lacus sollicitudin. Aliquam metus urna, elementum eu finibus eu, hendrerit maximus nunc. Sed id massa et nibh scelerisque semper in at magna. Suspendisse sed ultrices felis. Praesent felis leo, elementum id maximus eget, blandit vitae eros. Proin vel lacus lacus. Maecenas mauris purus, finibus a efficitur sed, lobortis sit amet augue. Morbi elementum massa at pulvinar tempus. Nullam suscipit nisl eu erat faucibus pretium. Aliquam sodales eget dolor vestibulum facilisis. Praesent consectetur, justo vitae molestie ullamcorper, nisl sapien iaculis mi, sit amet accumsan lorem felis non tellus.
-                Praesent vitae mollis sapien. Sed id neque sem. Aenean purus nunc, fermentum varius risus ut, porttitor blandit tellus. Sed fermentum consectetur lectus, nec iaculis nisi molestie eget. Vestibulum rutrum tincidunt arcu, sed malesuada nisi. Curabitur libero urna, pulvinar quis faucibus sit amet, venenatis nec odio. Ut vel nibh vehicula, porttitor mi nec, laoreet justo. Duis euismod pharetra leo, et ultrices lacus placerat at. Vestibulum lorem erat, euismod ac dolor luctus, pharetra pretium eros. Nulla sit amet consequat arcu, et aliquam ante. Integer iaculis, turpis at venenatis tincidunt, dolor purus tristique massa, id tincidunt ante lorem porttitor purus. Duis felis sapien, ullamcorper sit amet suscipit eu, tristique id nulla. Duis tristique dolor non consectetur ultrices. Curabitur eget libero lobortis, posuere erat sed, rhoncus mauris.
-                Nullam vel quam felis. Donec egestas interdum enim, quis hendrerit sem consequat vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed dictum posuere orci in consectetur. Proin vitae lacus euismod, blandit magna id, sollicitudin tortor. Vivamus at fermentum neque. Fusce a rutrum metus. Duis porta sollicitudin dui. In lobortis laoreet laoreet. Quisque ut mi at nisi scelerisque viverra non in eros. Integer aliquam ipsum nec elit mattis, a commodo sem porta.
+                <a class="underlined underlined--reverse black-to-white"
+                  >consectetur adipiscing elit</a
+                >. Nunc tincidunt velit nulla, id pharetra ex pellentesque et.
+                Ut at ante malesuada neque vulputate fringilla a eget leo. Nam
+                ultrices diam eu metus tristique, vel faucibus lacus
+                sollicitudin. Aliquam metus urna, elementum eu finibus eu,
+                hendrerit maximus nunc. Sed id massa et nibh scelerisque semper
+                in at magna. Suspendisse sed ultrices felis. Praesent felis leo,
+                elementum id maximus eget, blandit vitae eros. Proin vel lacus
+                lacus. Maecenas mauris purus, finibus a efficitur sed, lobortis
+                sit amet augue. Morbi elementum massa at pulvinar tempus. Nullam
+                suscipit nisl eu erat faucibus pretium. Aliquam sodales eget
+                dolor vestibulum facilisis. Praesent consectetur, justo vitae
+                molestie ullamcorper, nisl sapien iaculis mi, sit amet accumsan
+                lorem felis non tellus. Praesent vitae mollis sapien. Sed id
+                neque sem. Aenean purus nunc, fermentum varius risus ut,
+                porttitor blandit tellus. Sed fermentum consectetur lectus, nec
+                iaculis nisi molestie eget. Vestibulum rutrum tincidunt arcu,
+                sed malesuada nisi. Curabitur libero urna, pulvinar quis
+                faucibus sit amet, venenatis nec odio. Ut vel nibh vehicula,
+                porttitor mi nec, laoreet justo. Duis euismod pharetra leo, et
+                ultrices lacus placerat at. Vestibulum lorem erat, euismod ac
+                dolor luctus, pharetra pretium eros. Nulla sit amet consequat
+                arcu, et aliquam ante. Integer iaculis, turpis at venenatis
+                tincidunt, dolor purus tristique massa, id tincidunt ante lorem
+                porttitor purus. Duis felis sapien, ullamcorper sit amet
+                suscipit eu, tristique id nulla. Duis tristique dolor non
+                consectetur ultrices. Curabitur eget libero lobortis, posuere
+                erat sed, rhoncus mauris. Nullam vel quam felis. Donec egestas
+                interdum enim, quis hendrerit sem consequat vulputate. Orci
+                varius natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Sed dictum posuere orci in consectetur.
+                Proin vitae lacus euismod, blandit magna id, sollicitudin
+                tortor. Vivamus at fermentum neque. Fusce a rutrum metus. Duis
+                porta sollicitudin dui. In lobortis laoreet laoreet. Quisque ut
+                mi at nisi scelerisque viverra non in eros. Integer aliquam
+                ipsum nec elit mattis, a commodo sem porta.
               </span>
             </p>
           </div>
@@ -103,20 +160,23 @@
                 ></iframe>
               </div>
               <div class="right-part-cont">
-              <div class="upper-loc-cont">
-                <p id="y-r-here">Use o mapa para ver a localização.</p>
-                <img src="/src/assets/images/location.png" class="location-icon" />
-              </div>
-              
-              <div class="address-cont">
-                <p class="popout black-to-white">
-                  <span id="adress-pt-1">11 W 53rd St,</span>
-                  <br />
-                  <span id="adress-pt-2">New York, NY,</span>
-                  <br />
-                  <span id="adress-pt-3">10019, Estados Unidos</span>
-                </p>
-              </div>
+                <div class="upper-loc-cont">
+                  <p id="y-r-here">Use o mapa para ver a localização.</p>
+                  <img
+                    src="/src/assets/images/location.png"
+                    class="location-icon"
+                  />
+                </div>
+
+                <div class="address-cont">
+                  <p class="popout black-to-white">
+                    <span id="adress-pt-1">11 W 53rd St,</span>
+                    <br />
+                    <span id="adress-pt-2">New York, NY,</span>
+                    <br />
+                    <span id="adress-pt-3">10019, Estados Unidos</span>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -163,7 +223,7 @@ import Footer from "../shared/footer/Footer.vue";
 import DarkMode from "../shared/dark-mode/Dark-mode.vue";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {
     "meu-menu-bar": MenuBar,
     "meu-menu-items": MenuItems,
@@ -172,17 +232,16 @@ export default {
     "meu-footer": Footer,
     "meu-dark-mode": DarkMode
   },
-  data () {
+  data() {
     return {
       data: null
-    }
+    };
   },
   methods: {
     getMarkers() {
       var codigo = 1;
-       /*$("#event-name").text("jquery loaded"); isso deu certo*/
-      $.getJSON("https://localhost:5001/api/Eventos/1", function(result) 
-      {
+      /*$("#event-name").text("jquery loaded"); isso deu certo*/
+      $.getJSON("https://localhost:5001/api/Eventos/1", function(result) {
         /*alert("entrou aq"); deu certo so dps do login na api*/
         $.each(result, function(i, field) {
           alert("entrou aq");
@@ -191,7 +250,10 @@ export default {
           $("#event-info-nome").text(field.nome);
           $("#event-info-local").text("Endereço:" + field.endereco);
           $("#event-info-datas").text(
-            "Data:" + this.formatDate(field.dataEvento.substring(0, 10).replace("-", "/"))
+            "Data:" +
+              this.formatDate(
+                field.dataEvento.substring(0, 10).replace("-", "/")
+              )
           );
           $("#event-info-horarios").text("Horários:" + field.horarios);
           $("#of-web-link-eve").href = field.linkSiteOficial;
@@ -219,17 +281,17 @@ export default {
         });
       });
     },
-    formatDate(){
+    formatDate() {
       var datePart = input.match(/\d+/g),
-          year = datePart[0], // get only two digits
-          month = datePart[1],
-          day = datePart[2];
+        year = datePart[0], // get only two digits
+        month = datePart[1],
+        day = datePart[2];
 
-        return day + "/" + month + "/" + year;
+      return day + "/" + month + "/" + year;
     }
   },
-  mounted () {
-    this.getMarkers()
+  mounted() {
+    this.getMarkers();
   }
-}
+};
 </script>
