385873c1650f0101e7fae9983e1a870c2259e0fe
digo-smithh <smithrodrigues08@gmail.com>
2020-05-29 21:43:46 -0300

    edit

--- API-olympia/Controllers/RedirectController.cs
+++ API-olympia/Controllers/RedirectController.cs
@@ -19,6 +19,20 @@ namespace API_olympia.Controllers
             this.Repo = repo;
         }
 
+        [HttpPost("Cadastro")]
+        public async Task<IActionResult> Post(Usuarios model)
+        {
+            try
+            {
+                Armazenador.StringValueRoute = HttpContext.Request.Host.Value;
+                return RedirectToAction("post", "usuarios", model);
+            }
+            catch
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
         [HttpGet("Eventos/{idEvento}")]
         public async Task<IActionResult> Get(int idEvento)
         {
@@ -29,7 +43,7 @@ namespace API_olympia.Controllers
             }
             catch
             {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados no get(id).");
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
 
