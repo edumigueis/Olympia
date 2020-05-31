434c0f8ee47cb86cbf6a11d6b306f55dd70528fb
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-05-31 14:17:52 -0300

    Ajustes

--- Olympia/src/components/obra/Obra.vue
+++ Olympia/src/components/obra/Obra.vue
@@ -188,49 +188,6 @@ export default {
   methods: {
     formatDate(input) {},
     getMarkers() {
-      var codigo = 1;
-      /*$("#event-name").text("jquery loaded");
-      alert("ai");*/
-      /*$.getJSON("https://localhost:5001/api/Eventos/1", function(result) {
-        alert("entrou aq");
-        $.each(result, function(i, field) {
-          alert("entrou aq");
-          $("#event-name").text(field.nome);
-          alert(field.name);
-          $("#event-info-nome").text(field.nome);
-          $("#event-info-local").text("Endereço:" + field.endereco);
-          $("#event-info-datas").text(
-            "Data:" +
-              this.formatDate(
-                field.dataEvento.substring(0, 10).replace("-", "/")
-              )
-          );
-          $("#event-info-horarios").text("Horários:" + field.horarios);
-          $("#of-web-link-eve").href = field.linkSiteOficial;
-          $(".day").text(field.dataEvento.toString().substring(8, 10));
-          $(".month").text(
-            monthNames[parseInt(field.dataEvento.substring(5, 7)) - 1]
-              .substring(0, 3)
-              .toUpperCase()
-          );
-          $("#event-name").text(field.nome);
-          $("#adress-pt-1").text(
-            field.endereco.substring(0, field.endereco.indexOf("Cidade:", 3))
-          );
-          $("#adress-pt-2").text(
-            field.endereco.substring(
-              field.endereco.indexOf("Cidade:", 3),
-              field.endereco.indexOf("País:", 6)
-            )
-          );
-          $("#adress-pt-3").text(
-            field.endereco.substring(field.endereco.indexOf("País:", 6))
-          );
-          $("#desc-ev-wrapper").text(field.descricao);
-          $("#event-map").attr("src", field.localizacaoCoord);
-        });
-      });*/
-      /*alert("ya");*/
       var url = window.location.href;
       var codigo = url.substring(29);
       $.ajax({
