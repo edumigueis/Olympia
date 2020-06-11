1458f18f89fb8de1d98081b6b93a252ca11e8603
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-06-11 17:22:19 -0300

    Melhorias na Sobre

--- Olympia/src/components/sobre/Sobre.vue
+++ Olympia/src/components/sobre/Sobre.vue
@@ -10,17 +10,18 @@
       <meu-mouse></meu-mouse>
       <meu-dark-mode></meu-dark-mode>
 
-       <span id="fundo">
+      <span id="fundo">
         <div class="part-2 white-4">
           <div class="gradient gradient-sobre"></div>
           <div class="bg"></div>
           <div class="title">
             <h1 class="page-title">SOBRE<br />NÓS</h1>
           </div>
-          <div class="bg"></div> -->
+          <div class="bg"></div>
+          -->
 
-                            <!--BACKGROUND-->
-                            <!-- <div style="position:relative">
+          <!--BACKGROUND-->
+          <!-- <div style="position:relative">
                               <div class="image-container">
                                 <img src="/src/assets/images/sede.jpg" alt class="image" />
                               </div>
@@ -29,52 +30,221 @@
                                 Conheça um pouco mais sobre nós
                               </h2>
                             </div> -->
+          <div class="wrapper-sb white-7" style="overflow: hidden;">
+            <video
+              src="/src/assets/videos/sb-video1.mp4"
+              autoplay
+              muted
+              playsinline
+              loop
+              has-binding=""
+            ></video>
+            <h1 class="slogan">Share your art. Share your love.</h1>
+          </div>
+          <div class="wrapper-sb white-7 wrapper-over" id="wrapper-rest">
+            <div class="main-text">
+              <h1 class="text-title-principal">Olympia</h1>
+              <p class="sb-mission">
+                Somos uma corporação que valoriza a arte sua completa e em todas
+                as suas formas. Acreditamos em tudo que a arte representa: como
+                ela embeleza a vida, trazendo mais cor e sentido, e também como
+                ela pode representar resistência em momentos tão dificeis na
+                humanidade.
+              </p>
+            </div>
+            <div class="bottom-wrapper-sb">
+              <div class="colunm">
+                <h1 class="stories-title">Histórias e Momentos</h1>
+                <ul class="grid">
+                  <li>
+                    <figure class="grid__figure">
+                      <img src="https://picsum.photos/300/200/?random" alt="" />
+                      <p class="action-taker-p">Olympia</p>
+                      <figcaption>
+                        Como o Olympia está ajudando a informar as pessoas sobre
+                        o coronavírus.
+                      </figcaption>
+                    </figure>
+                  </li>
+                  <li>
+                    <figure class="grid__figure">
+                      <img src="https://picsum.photos/200/800/?random" alt="" />
+                      <p class="action-taker-p">Olympia</p>
+                      <figcaption>
+                        Aproximando pessoas. Assista ao episódio 6 no YouTube do
+                        Olympia.
+                      </figcaption>
+                    </figure>
+                  </li>
+                  <li>
+                    <figure class="grid__figure">
+                      <img src="https://picsum.photos/200/300/?random" alt="" />
+                      <p class="action-taker-p">Olympia</p>
+                      <figcaption>
+                        Tornando o trabalho dos artistas mais fácil. Aproximando
+                        artistas.
+                      </figcaption>
+                    </figure>
+                  </li>
+                  <li>
+                    <figure class="grid__figure">
+                      <img src="https://picsum.photos/400/200/?random" alt="" />
+                      <p class="action-taker-p">Olympia</p>
+                      <figcaption>
+                        Olympia pela diversidde: acompanhe nossos projetos.
+                      </figcaption>
+                    </figure>
+                  </li>
+                  <span class="divi">
+                    <li>
+                      <figure class="grid__figure">
+                        <img
+                          src="https://picsum.photos/600/600/?random"
+                          alt=""
+                        />
+                        <p class="action-taker-p">Olympia</p>
+                        <figcaption>
+                          Aproximando pessoas. Assista ao episódio 7 no YouTube
+                          do Olympia.
+                        </figcaption>
+                      </figure>
+                    </li>
+                  </span>
+                  <span class="divi">
+                    <li>
+                      <figure class="grid__figure">
+                        <img
+                          src="https://picsum.photos/640/400/?random"
+                          alt=""
+                        />
+                        <p class="action-taker-p">Olympia</p>
+                        <figcaption>
+                          Siga o perfil oficial do Olympia.
+                        </figcaption>
+                      </figure>
+                    </li>
+                  </span>
+                </ul>
+              </div>
+              <div class="colunm">
+                <div class="iphone iphone-1">
+                  <div class="iphone-top">
+                    <span class="camera"></span>
+                    <span class="sensor"></span>
+                    <span class="speaker"></span>
+                  </div>
+                  <div class="top-bar"></div>
+                  <div class="iphone-screen"></div>
+                  <div class="buttons">
+                    <span class="on-off"></span>
+                    <span class="sleep"></span>
+                    <span class="up"></span>
+                    <span class="down"></span>
+                  </div>
+                  <div class="bottom-bar"></div>
+                  <div class="iphone-bottom">
+                    <span></span>
+                  </div>
+                </div>
+                <div class="iphone iphone-2">
+                  <div class="iphone-top">
+                    <span class="camera"></span>
+                    <span class="sensor"></span>
+                    <span class="speaker"></span>
+                  </div>
+                  <div class="top-bar"></div>
+                  <div class="iphone-screen"></div>
+                  <div class="buttons">
+                    <span class="on-off"></span>
+                    <span class="sleep"></span>
+                    <span class="up"></span>
+                    <span class="down"></span>
+                  </div>
+                  <div class="bottom-bar"></div>
+                  <div class="iphone-bottom">
+                    <span></span>
+                  </div>
+                </div>
+                <ul class="grid-inline">
+                  <li>
+                    <figure class="grid__figure">
+                      <img src="https://picsum.photos/600/600/?random" alt="" />
+                      <p class="action-taker-p">Olympia</p>
+                      <figcaption>
+                        Aproximando pessoas. Assista ao episódio 7 no YouTube do
+                        Olympia.
+                      </figcaption>
+                    </figure>
+                  </li>
+                  <li>
+                    <figure class="grid__figure">
+                      <img src="https://picsum.photos/640/400/?random" alt="" />
+                      <p class="action-taker-p">Olympia</p>
+                      <figcaption>
+                        Siga o perfil oficial do Olympia. E, não perca nada que
+                        nós postamos. Nosso time trabalha muito para sempre
+                        trazer os melhores conteúdos para você.
+                      </figcaption>
+                    </figure>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="wrapper-sb newsroom">
+            <h1 class="newsroom-title">Newsroom</h1>
+            <div class="container">
+              <div class="box">
+                <img src="https://source.unsplash.com/1000x800" />
+                <span>New Olympia Version</span>
+              </div>
+              <div class="box">
+                <img src="https://source.unsplash.com/1000x802" />
+                <span>Whats new in 1.2?</span>
+              </div>
+              <div class="box">
+                <img src="/src/assets/images/newsroom2.jpeg" />
+                <span>The Olympia Mission</span>
+              </div>
+              <div class="box">
+                <img src="/src/assets/images/newsroom1.jpg" />
+                <span>What we are up to</span>
+              </div>
+            </div>
+          </div>
 
-          <div class="container-left show-on-scroll black-to-white white-7 first">
-          <table>
-            <tr>
-              <td class="texto-principal">
-                <h1 class="text-title-principal">QUEM SOMOS?</h1>
-                Olympia: somos uma corporação que valoriza a arte sua completa e em todas as suas formas.
-                Acreditamos em tudo que a arte representa: como ela embeleza a vida, trazendo mais cor e sentido, e também como
-                ela pode representar resistência em momentos tão dificeis na humanidade.
-              </td>
-            </tr>
-          </table>
-        </div>
+          <!--<img class="linha" src="https://img.pngio.com/black-line-png-just-pink-about-it-black-line-png-900_150.png"/>-->
 
-        <!--<img class="linha" src="https://img.pngio.com/black-line-png-just-pink-about-it-black-line-png-900_150.png"/>-->
-
-        <div class="container-right show-on-scroll black-to-white white-7">
-          <table>
-            <tr>
-              <td class="texto-linha">
-                <h1 class="text-title">SEDE</h1>
-                Nossa sede se encontra na cidade de Stuttgart na Alemanha, projeto é de James Stirling e antes abrigava um museu.
-                Temos muito orgulho de ter esse projeto arquitetônico como nosso patrimônio.
-               
-              </td>
-              
-            </tr>
-          </table>
-        </div>
+          <div class="container-right show-on-scroll black-to-white white-7">
+            <table>
+              <tr>
+                <td class="texto-linha">
+                  <h1 class="text-title">SEDE</h1>
+                  Nossa sede se encontra na cidade de Stuttgart na Alemanha,
+                  projeto é de James Stirling e antes abrigava um museu. Temos
+                  muito orgulho de ter esse projeto arquitetônico como nosso
+                  patrimônio.
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
         <div class="container-left show-on-scroll black-to-white white-7 first">
           <table>
             <tr>
               <td class="texto-linha">
-                Nenhum artista suporta o real. Qualquer artista, olhando para trás, tem a sincera opinião de que o verdadeiro valor de uma coisa é uma «sombra» que pode ser fixada nas cores, na forma, no som, no pensamento.
-                -Friedrich Wilhelm Nietzsche
+                Nenhum artista suporta o real. Qualquer artista, olhando para
+                trás, tem a sincera opinião de que o verdadeiro valor de uma
+                coisa é uma «sombra» que pode ser fixada nas cores, na forma, no
+                som, no pensamento. -Friedrich Wilhelm Nietzsche
               </td>
             </tr>
           </table>
         </div>
-        
-       
-      </span> 
+      </span>
     </div>
 
-      <!--<div id="text" class='text-sobre'>
+    <!--<div id="text" class='text-sobre'>
         <p id="title">NOSSA HISTÓRIA</p>
         <div class="linha linha-sobre"></div>
         <p id="texto1">
@@ -135,8 +305,7 @@
         </p>
         <div class="linha linha-sobre"></div>
       </div>-->
-      <meu-footer></meu-footer>
-    
+    <meu-footer></meu-footer>
   </main>
 </template>
 <script>
